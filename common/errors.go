f4045625c122cac542c2dfeb40a3a2a1201eba93
Hyunil Jung <hijung@icon.foundation>
2018-10-22 17:30:15 +0900

    Added Unsupported error.

--- common/errors.go
+++ common/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrorUnknown         = errors.New("Unknown error")
 	ErrorIllegalArgument = errors.New("Illegal argument")
 	ErrorInvalidState    = errors.New("Invalid state")
+	ErrorUnsupported     = errors.New("Unsupported")
 )
