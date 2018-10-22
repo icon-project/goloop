cb3dc16524904b749a53288fb51c1ac688c90ca8
Hyunil Jung <hijung@icon.foundation>
2018-10-22 17:29:49 +0900

    Updated module APIs.

--- module/block.go
+++ module/block.go
@@ -1,5 +1,7 @@
 package module
 
+import "io"
+
 type Address interface {
 	String() string
 	Bytes() []byte
@@ -10,28 +12,41 @@ type Vote interface {
 	Bytes() []byte
 }
 
+type Validator Address
+
 type Block interface {
+	Version() int
 	ID() []byte
 	Height() int64
 	PrevRound() int
 	PrevID() []byte
 	Votes() []Vote
-	NextValidators() []Address
+	NextValidators() []Validator
+	//	TODO remove
 	Verify() error
+	NormalTransactions() TransactionList
+	PatchTransactions() TransactionList
+	Timestamp() int64
+	Proposer() Validator
 }
 
 type BlockManager interface {
+	GetBlock(id []byte) Block
+
 	//	Propose proposes a Block following the parent Block.
 	//	The result is asynchronously notified by cb. canceler cancels the
 	//	operation. canceler returns true and cb is not called if the
 	//	cancellation was successful.
-	Propose(parent Block, votes []Vote, cb func(Block, error)) (canceler func() bool, err error)
+	Propose(parentID []byte, votes []Vote, cb func(Block, error)) (canceler func() bool, err error)
 
 	//	Import creates a Block from blockBytes.
 	//	The result is asynchronously notified by cb. canceler cancels the
 	//	operation. canceler returns true and cb is not called if the
 	//	cancellation was successful.
-	Import(blockBytes []byte, cb func(Block, error)) (canceler func() bool, err error)
+	Import(r io.Reader, cb func(Block, error)) (canceler func() bool, err error)
+
 	Commit(Block) error
+
+	//	Finalize updates world state according to Block block and removes non-finalized committed blocks with the same height as block from persistent storage.
 	Finalize(Block) error
 }
