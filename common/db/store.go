5ef470eb99797f8ca6cb8aeb33f68f2092eed3c1
Hyunil Jung <hijung@icon.foundation>
2018-10-25 14:09:19 +0900

    Updated db APIs.

--- common/db/store.go
+++ common/db/store.go
@@ -1,7 +1,41 @@
 package db
 
+//	Database IDs.
+const (
+	// StateTrie maps account from sha3(address)
+	StateTrie = ""
+	// StorageTrie maps storage value from storage key
+	StorageTrie = ""
+	// NormalTransactionTrie maps transaction from index
+	NormalTransactionTrie = ""
+	// PatchTransactionTrie maps transaction from index
+	PatchTranscationTrie = ""
+	// NormalReceiptTrie maps receipt from index
+	NormalReceiptTrie = ""
+	// PatchReceiptTrie maps receipt from index
+	PatchReceiptTrie = ""
+	// BlockHeaderByHash maps block header from hash of encoded block header.
+	BlockHeaderByHash = "S"
+	// SignaturesByHash maps signature array from hash of encoded signature
+	// array.
+	SignaturesByHash = "S"
+	// ValidatorsByHash maps validator array from hash of encoded validator
+	// array.
+	ValidatorsByHash = "S"
+	// CodeByHash maps code from hash of code
+	CodeByHash = "S"
+	// TransactionLocatorByHash maps transaction locator from transaction hash.
+	TransactionLocatorByHash = "T"
+	// BlockHeaderHashByHeight maps hash of encoded block header from height.
+	BlockHeaderHashByHeight = "H"
+	// BlockV1ByHash maps block V1 from block V1 hash.
+	BlockV1ByHash = "B"
+	// BlockV1ByHash maps receipt V1 from tx V3 hash.
+	ReceiptV1ByHash = "R"
+)
+
 type Store interface {
-	GetDB(id []byte) (DB, error)
+	GetDB(id string) (DB, error)
 }
 
 func GetStore(name string) (Store, error) {
