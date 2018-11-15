e5cb8eb1347e0000199520b9e8623475fd1fa4a2
Charles CW Kwak <cwkwak@icon.foundation>
2018-11-15 18:26:32 +0900

    Add account and transaction creation for test.

--- service/manager_test.go
+++ service/manager_test.go
@@ -1,9 +1,11 @@
 package service_test
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/icon-project/goloop/common"
 	"github.com/icon-project/goloop/common/codec"
+	"github.com/icon-project/goloop/common/crypto"
 	"github.com/icon-project/goloop/common/db"
 	"github.com/icon-project/goloop/common/trie"
 	"github.com/icon-project/goloop/common/trie/trie_manager"
@@ -20,8 +22,8 @@ type txTest struct {
 	group module.TransactionGroup
 
 	version   int
-	from      common.Address
-	to        common.Address
+	from      *common.Address
+	to        *common.Address
 	value     *big.Int
 	stepLimit *big.Int
 	timestamp int64
@@ -31,6 +33,28 @@ type txTest struct {
 
 	hash  []byte
 	bytes []byte
+}
+
+type txTestV2 struct {
+	from      common.Address  `json:"from"`
+	to        common.Address  `json:"to"`
+	value     common.HexInt   `json:"value"`
+	fee       common.HexInt   `json:"fee"`
+	timestamp common.HexInt64 `json:"timestamp"`
+	nonce     common.HexInt64 `json:"nonce"`
+	signature common.HexBytes `json:"signature"`
+}
+
+type txTestV3 struct {
+	version   common.HexInt16 `json:"version"`
+	from      common.Address  `json:"from"`
+	to        common.Address  `json:"to"`
+	value     common.HexInt   `json:"value"`
+	stepLimit common.HexInt   `json:"stepLimit"`
+	timestamp common.HexInt64 `json:"timestamp"`
+	nid       common.HexInt16 `json:"nid"`
+	nonce     common.HexInt64 `json:"nonce"`
+	signature common.HexBytes `json:"signature"`
 }
 
 func (tx *txTest) Group() module.TransactionGroup {
@@ -54,11 +78,11 @@ func (tx *txTest) Verify() error {
 }
 
 func (tx *txTest) From() module.Address {
-	return &tx.from
+	return tx.from
 }
 
 func (tx *txTest) To() module.Address {
-	return &tx.to
+	return tx.to
 }
 
 func (tx *txTest) Value() *big.Int {
@@ -142,47 +166,132 @@ var toList = []string{
 //var addresses [10]common.Address
 var deposit = int64(1000000)
 
-var testAddresses [][]byte
+var testWallets []testWallet
 
 const (
-	TEST_ACCOUNTS             = 10
+	TEST_ACCOUNTS             = 17
 	TEST_VALID_REQUEST_TX_NUM = 100
 )
 
 type testWallet struct {
-	pbKey   []byte
-	prKey   []byte
-	address []byte
+	pbKey   *crypto.PublicKey
+	prKey   *crypto.PrivateKey
+	address *common.Address
 }
 
-// will be implemented by cw.Kwak
 func createWallet(walletNum int) []testWallet {
-	return []testWallet{}
+	ws := make([]testWallet, walletNum)
+	for i := 0; i < walletNum; i++ {
+		w := testWallet{}
+		w.prKey, w.pbKey = crypto.GenerateKeyPair()
+		w.address = common.NewAccountAddressFromPublicKey(w.pbKey)
+		ws[i] = w
+	}
+	return ws
 }
 
-// will be implemented by cw.Kwak
-func createTxInst(from, to common.Address, value *big.Int, timestamp int64) module.Transaction {
-	return nil
+func createTxInst(wallet *testWallet, to *common.Address, value *big.Int, timestamp int64) module.Transaction {
+	r := rand.Int63()
+	ver := int((r % 2) + 2)
+
+	tx := txTest{}
+	tx.group = module.TransactionGroupNormal
+	tx.version = ver
+	tx.from = wallet.address
+	tx.to = to
+	tx.value = value
+	tx.stepLimit = new(big.Int).SetInt64(r % 0xffff)
+	tx.timestamp = timestamp
+	tx.nid = 0
+	tx.nonce = r
+
+	// create a signature
+	m := make(map[string]interface{})
+	switch tx.version {
+	case 2:
+		m["from"] = *tx.from
+		m["to"] = *tx.to
+		m["value"] = common.HexInt{*tx.value}
+		m["fee"] = common.HexInt{*tx.stepLimit}
+		m["nonce"] = common.HexInt64{tx.nonce}
+	case 3:
+		m["version"] = common.HexInt16{int16(tx.version)}
+		m["from"] = *tx.from
+		m["to"] = *tx.to
+		m["value"] = common.HexInt{*tx.value}
+		m["stepLimit"] = common.HexInt{*tx.stepLimit}
+		m["nonce"] = common.HexInt64{tx.nonce}
+	default:
+		log.Fatalln("unknown transaction version:", tx.version)
+	}
+	bs, err := service.SerializeMap(m, map[string]bool(nil), map[string]bool(nil))
+	if err != nil {
+		log.Fatalln("fail to create transaction bytes")
+	}
+	bs = append([]byte("icx_sendTransaction."), bs...)
+	h := crypto.SHA3Sum256(bs)
+	sig, err := crypto.NewSignature(h, wallet.prKey)
+	if err != nil {
+		log.Fatalln("fail to create a signature")
+	}
+	tx.signature, _ = sig.SerializeRSV()
+
+	// create bytes
+	tx.bytes = marshalTx(&tx)
+
+	return &tx
+}
+
+func marshalTx(tx *txTest) []byte {
+	var i interface{}
+	switch tx.version {
+	case 2:
+		ti := &txTestV2{
+			from:      *tx.from,
+			to:        *tx.to,
+			value:     common.HexInt{*tx.value},
+			fee:       common.HexInt{*tx.stepLimit},
+			timestamp: common.HexInt64{tx.timestamp},
+			nonce:     common.HexInt64{tx.nonce},
+			signature: tx.signature,
+		}
+		i = ti
+	case 3:
+		ti := &txTestV3{
+			version:   common.HexInt16{int16(tx.version)},
+			from:      *tx.from,
+			to:        *tx.to,
+			value:     common.HexInt{*tx.value},
+			stepLimit: common.HexInt{*tx.stepLimit},
+			timestamp: common.HexInt64{tx.timestamp},
+			nid:       common.HexInt16{int16(tx.nid)},
+			nonce:     common.HexInt64{tx.nonce},
+			signature: tx.signature,
+		}
+		i = ti
+	}
+	b, _ := json.Marshal(i)
+	return b
 }
 
 //const txLiveDuration = int64(60 * time.Second / time.Millisecond) // 60 seconds in millisecond
 //// true if valid transaction
 func createRandTx(valid bool, time int64, validNum int) module.Transaction {
-	id := rand.Int() % toNum
+	idFrom := rand.Int() % toNum
 	//tx.hash = []byte{id}
 	// valid 하도록 만든다. 기존에 없는 ID, time 등을 이용하도록.
 	// insert transaction to valid transaction (expected txPool).
 	// ID map, time map 사용.
 	// 중복될 경우 새로운 ID, time을 생성한다.
-	toId := rand.Int() % toNum
-	for toId == id {
-		toId = rand.Int() % toNum
+	idTo := rand.Int() % toNum
+	for idTo == idFrom {
+		idTo = rand.Int() % toNum
 	}
 	//tx.to = addresses[toId]
-	fromString := string(testAddresses[id])
-	toString := string(testAddresses[toId])
-	from := *common.NewAccountAddress([]byte(testAddresses[id]))
-	to := *common.NewAccountAddress([]byte(toList[toId]))
+	fromString := testWallets[idFrom].address.String()
+	toString := testWallets[idTo].address.String()
+	walletFrom := testWallets[idTo]
+	addrTo := testWallets[idTo].address
 	value := big.NewInt(int64(rand.Int() % 300000))
 
 	if valid {
@@ -198,7 +307,7 @@ func createRandTx(valid bool, time int64, validNum int) module.Transaction {
 
 		timestamp := time + 1000 + int64(rand.Int()%100)
 		// TODO: check value type. no
-		return createTxInst(from, to, value, timestamp)
+		return createTxInst(&walletFrom, addrTo, value, timestamp)
 	}
 	//invalid하도록 만든다.
 	// ID를 map에서 가져다가 쓰거나 전달받은 시간보다 작은 시간을 설정한다.
@@ -206,7 +315,7 @@ func createRandTx(valid bool, time int64, validNum int) module.Transaction {
 	// sleep을 줄까...
 	// TODO: ADD verify
 	timestamp := time - service.TestTxLiveDuration() - 1000 - int64(rand.Int()%10)
-	return createTxInst(from, to, value, timestamp)
+	return createTxInst(&walletFrom, addrTo, value, timestamp)
 }
 
 func makeTimestamp() int64 {
@@ -237,13 +346,12 @@ func requestTx(validTxNum int, manager module.ServiceManager, done chan bool) {
 // create wallet(private/public keys & address) and set ballance
 // then set addresses and accounts to trie
 func initTestWallet(db db.Database, mpt trie.Mutable) {
-	wallet := createWallet(TEST_ACCOUNTS)
+	testWallets = createWallet(TEST_ACCOUNTS)
 	for i := 0; i < TEST_ACCOUNTS; i++ {
-		testAddresses[i] = wallet[i].address
 		accountState := service.TestNewAccountState(db)
 		accountState.SetBalance(big.NewInt(deposit))
 		serializedAccount, _ := codec.MP.MarshalToBytes(accountState.GetSnapshot())
-		addr := *common.NewAccountAddress(testAddresses[i])
+		addr := testWallets[i].address
 		mpt.Set(addr.Bytes(), serializedAccount)
 	}
 }
