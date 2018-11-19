7dc3aeac1d440fd4bf9e9fd904ba7ecd3d3f48db
MoonKyu Song <mksong@icon.foundation>
2018-11-19 14:55:59 +0900

    Implement transaction list v1.
    Legacy block system uses different merkle root calculation algorithm.
    So, I have added it to "service" module.
    And also let it be used by sample test codes.

--- cmd/testchain/block_v1.go
+++ cmd/testchain/block_v1.go
@@ -17,11 +17,32 @@ import (
 	"github.com/icon-project/goloop/service"
 )
 
+type transactionV3 struct {
+	module.Transaction
+}
+
+func (t *transactionV3) MarshalJSON() ([]byte, error) {
+	return nil, nil
+}
+
+func (t *transactionV3) UnmarshalJSON(b []byte) error {
+	if tr, err := service.NewTransactionV3(b); err != nil {
+		return err
+	} else {
+		t.Transaction = tr
+		return nil
+	}
+}
+
+func (t transactionV3) String() string {
+	return fmt.Sprint(t.Transaction)
+}
+
 type blockV1Impl struct {
 	Version            string             `json:"version"`
 	PrevBlockHash      common.RawHexBytes `json:"prev_block_hash"`
 	MerkleTreeRootHash common.RawHexBytes `json:"merkle_tree_root_hash"`
-	Transactions       []transaction      `json:"confirmed_transaction_list"`
+	Transactions       []transactionV3    `json:"confirmed_transaction_list"`
 	BlockHash          common.RawHexBytes `json:"block_hash"`
 	Height             int64              `json:"height"`
 	PeerID             string             `json:"peer_id"`
@@ -31,6 +52,7 @@ type blockV1Impl struct {
 
 type blockV1 struct {
 	*blockV1Impl
+	transactionList module.TransactionList
 }
 
 func (b *blockV1) Version() int {
@@ -62,7 +84,37 @@ func (b *blockV1) NextValidators() module.ValidatorList {
 }
 
 func (b *blockV1) Verify() error {
-	return b.blockV1Impl.Verify()
+	bs := make([]byte, 128+8)
+	copy(bs[0:], []byte(b.PrevBlockHash.String()))
+	copy(bs[64:], []byte(b.MerkleTreeRootHash.String()))
+	binary.LittleEndian.PutUint64(bs[128:], b.TimeStamp)
+	bhash := crypto.SHA3Sum256(bs)
+
+	if bytes.Compare(bhash, b.BlockHash) != 0 {
+		log.Println("RECORDED  ", b.BlockHash)
+		log.Println("CALCULATED", hex.EncodeToString(bhash))
+		return errors.New("HASH is incorrect")
+	}
+
+	if pk, err := b.Signature.RecoverPublicKey(bhash); err == nil {
+		addr := common.NewAccountAddressFromPublicKey(pk).String()
+		if addr != b.PeerID {
+			log.Println("PEERID    ", b.PeerID)
+			log.Println("SIGNER    ", addr)
+			return errors.New("SIGNER is different from PEERID")
+		}
+	} else {
+		log.Println("FAIL to recover address from signature")
+		return err
+	}
+
+	mrh := b.NormalTransactions().Hash()
+	if bytes.Compare(mrh, b.MerkleTreeRootHash) != 0 {
+		log.Println("MerkleRootHash STORE", hex.EncodeToString(b.MerkleTreeRootHash))
+		log.Println("MerkleRootHash CALC ", hex.EncodeToString(mrh))
+		return errors.New("MerkleTreeRootHash is different")
+	}
+	return nil
 }
 
 func (b *blockV1) String() string {
@@ -70,11 +122,7 @@ func (b *blockV1) String() string {
 }
 
 func (b *blockV1) NormalTransactions() module.TransactionList {
-	trs := make([]module.Transaction, len(b.blockV1Impl.Transactions))
-	for i, tx := range b.blockV1Impl.Transactions {
-		trs[i] = tx
-	}
-	return service.NewTransactionListFromSlice(trs)
+	return b.transactionList
 }
 
 func (b *blockV1) PatchTransactions() module.TransactionList {
@@ -119,74 +167,10 @@ func NewBlockV1(b []byte) (module.Block, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &blockV1{blk}, nil
-}
-
-func calcMergedHash(h1, h2 []byte) []byte {
-	var b [128]byte
-	copy(b[0:], []byte(hex.EncodeToString(h1)))
-	copy(b[64:], []byte(hex.EncodeToString(h2)))
-	ts := crypto.SHASum256(b[:])
-	return ts[:]
-}
-
-func calcMerkleTreeRoot(m [][]byte) []byte {
-	if len(m) == 0 {
-		var empty [32]byte
-		return empty[:]
+	trs := make([]module.Transaction, len(blk.Transactions))
+	for i, tx := range blk.Transactions {
+		trs[i] = tx
 	}
-	ml := make([][]byte, len(m))
-	copy(ml, m)
-	for mlen := len(ml); mlen > 1; mlen = (mlen + 1) / 2 {
-		for i := 0; i < mlen; i += 2 {
-			if i+1 < mlen {
-				ml[i/2] = calcMergedHash(ml[i], ml[i+1])
-			} else {
-				ml[i/2] = calcMergedHash(ml[i], ml[i])
-			}
-		}
-	}
-	return ml[0]
-}
-
-func (blk *blockV1Impl) Verify() error {
-	b := make([]byte, 128+8)
-	copy(b[0:], []byte(blk.PrevBlockHash.String()))
-	copy(b[64:], []byte(blk.MerkleTreeRootHash.String()))
-	binary.LittleEndian.PutUint64(b[128:], blk.TimeStamp)
-	bhash := crypto.SHA3Sum256(b)
-
-	if bytes.Compare(bhash, blk.BlockHash) != 0 {
-		log.Println("RECORDED  ", blk.BlockHash)
-		log.Println("CALCULATED", hex.EncodeToString(bhash))
-		return errors.New("HASH is incorrect")
-	}
-
-	if pk, err := blk.Signature.RecoverPublicKey(bhash); err == nil {
-		addr := common.NewAccountAddressFromPublicKey(pk).String()
-		if addr != blk.PeerID {
-			log.Println("PEERID    ", blk.PeerID)
-			log.Println("SIGNER    ", addr)
-			return errors.New("SIGNER is different from PEERID")
-		}
-	} else {
-		log.Println("FAIL to recover address from signature")
-		return err
-	}
-
-	merkle := make([][]byte, len(blk.Transactions))
-	for i, t := range blk.Transactions {
-		if err := t.Verify(); err != nil {
-			log.Printf("Transaction[%d] Verification fails", i)
-			return err
-		}
-		merkle[i] = t.ID()
-	}
-	mrh := calcMerkleTreeRoot(merkle)
-	if bytes.Compare(mrh, blk.MerkleTreeRootHash) != 0 {
-		log.Println("MerkleRootHash STORE", hex.EncodeToString(blk.MerkleTreeRootHash))
-		log.Println("MerkleRootHash CALC ", hex.EncodeToString(mrh))
-		return errors.New("MerkleTreeRootHash is different")
-	}
-	return nil
+	transactionList := service.NewTransactionListV1FromSlice(trs)
+	return &blockV1{blk, transactionList}, nil
 }
---
+++ service/transactionlist_v1.go
@@ -0,0 +1,97 @@
+package service
+
+import (
+	"bytes"
+	"encoding/hex"
+	"github.com/icon-project/goloop/common"
+	"github.com/icon-project/goloop/common/crypto"
+	"github.com/icon-project/goloop/module"
+)
+
+type TransactionListV1 struct {
+	list []module.Transaction
+	hash []byte
+}
+
+func (l *TransactionListV1) Get(i int) (module.Transaction, error) {
+	if i >= 0 && i < len(l.list) {
+		return l.list[i], nil
+	}
+	return nil, common.ErrNotFound
+}
+
+type transactionListV1Iterator struct {
+	list []module.Transaction
+	idx  int
+}
+
+func (i *transactionListV1Iterator) Get() (module.Transaction, int, error) {
+	if i.idx >= len(i.list) {
+		return nil, 0, common.ErrInvalidState
+	}
+	return i.list[i.idx], i.idx, nil
+}
+
+func (i *transactionListV1Iterator) Has() bool {
+	return i.idx < len(i.list)
+}
+
+func (i *transactionListV1Iterator) Next() error {
+	if i.idx < len(i.list) {
+		i.idx++
+		return nil
+	} else {
+		return common.ErrInvalidState
+	}
+}
+
+func (l *TransactionListV1) Iterator() module.TransactionIterator {
+	return &transactionListV1Iterator{
+		list: l.list,
+		idx:  0,
+	}
+}
+
+func calcMergedHash(h1, h2 []byte) []byte {
+	var b [128]byte
+	copy(b[0:], []byte(hex.EncodeToString(h1)))
+	copy(b[64:], []byte(hex.EncodeToString(h2)))
+	ts := crypto.SHASum256(b[:])
+	return ts[:]
+}
+
+func calcMerkleTreeRoot(m [][]byte) []byte {
+	if len(m) == 0 {
+		var empty [32]byte
+		return empty[:]
+	}
+	ml := make([][]byte, len(m))
+	copy(ml, m)
+	for mlen := len(ml); mlen > 1; mlen = (mlen + 1) / 2 {
+		for i := 0; i < mlen; i += 2 {
+			if i+1 < mlen {
+				ml[i/2] = calcMergedHash(ml[i], ml[i+1])
+			} else {
+				ml[i/2] = calcMergedHash(ml[i], ml[i])
+			}
+		}
+	}
+	return ml[0]
+}
+
+func (l *TransactionListV1) Hash() []byte {
+	return l.hash
+}
+
+func (l *TransactionListV1) Equal(t module.TransactionList) bool {
+	return bytes.Equal(l.Hash(), t.Hash())
+}
+
+func NewTransactionListV1FromSlice(list []module.Transaction) module.TransactionList {
+	ml := make([][]byte, len(list))
+	for i, t := range list {
+		ml[i] = t.ID()
+	}
+	hash := calcMerkleTreeRoot(ml)
+	return &TransactionListV1{list: list, hash: hash}
+}
