6c62520f3472965ac7ae3e496278225428640710
MoonKyu Song <mksong@icon.foundation>
2018-11-29 10:20:58 +0900

    Feature Python Execution Engine Proxy.
    Python library used for implementing Python Execution Engine.

---
+++ pyee/ipc/client.py
@@ -0,0 +1,43 @@
+import socket
+from typing import Any
+from typing import Tuple
+import msgpack
+
+
+class Client:
+    def __init__(self) -> None:
+        self.conn = None
+        self.req_sn: int = 0
+        self.unpacker = None
+
+    def _send(self, msg: int, data: Any):
+        msg_to_send = [msg, data]
+        msgpack.dump(msg_to_send, self)
+
+    def _recv(self) -> Tuple[int, Any]:
+        msg = self.unpacker.unpack()
+        return msg[0], msg[1]
+
+    def connect(self, address: str) -> None:
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sock.connect(address)
+        self.conn = sock
+        self.unpacker = msgpack.Unpacker(self)
+
+    def write(self, b: bytes) -> None:
+        self.conn.sendall(b)
+
+    def read(self, n=None) -> bytes:
+        if n is None:
+            n = 1024
+        return self.conn.recv(n)
+
+    def send(self, msg: int, data: Any):
+        self._send(msg, 0, data)
+
+    def send_and_receive(self, msg: int, data: Any) -> Tuple[int, int, Any]:
+        self._send(msg, data)
+        return self._recv()
+
+    def receive(self) -> Tuple[int, int, Any]:
+        return self._recv()
---
+++ pyee/ipc/test_execution_handler.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+from .execution_handler import *
+
+
+class TestExecutionHandler(TestCase):
+    def test_int_to_bytes(self):
+        cases = [
+            (0, [0x00]),
+            (0x80, [0x00, 0x80]),
+            (-0x80, [0x80]),
+            (-255, [0xff, 0x01]),
+            (0x1234, [0x12, 0x34]),
+        ]
+        for c, b in cases:
+            b2 = int_to_bytes(c)
+            if b2 != bytes(b):
+                self.fail(f"Expect {b} but {b2} returned for {c}")
+
+    def test_bytes_to_int(self):
+        cases = [
+            0,
+            0x7f,
+            0x80,
+            -0x80,
+            -0xff,
+            0x7fff,
+            0x7fffffff,
+        ]
+        for c in cases:
+            bs = int_to_bytes(c)
+            c_out = bytes_to_int(bs)
+            if c_out != c:
+                self.fail()
