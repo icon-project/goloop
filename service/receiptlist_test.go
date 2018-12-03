0705f6ec044e9006f4d020b05224905ca80f312b
MoonKyu Song <mksong@icon.foundation>
2018-12-04 08:12:45 +0900

    Update receiptlist_test.go.

--- service/receiptlist_test.go
+++ service/receiptlist_test.go
@@ -2,80 +2,57 @@ package service
 
 import (
 	"bytes"
+	"github.com/icon-project/goloop/common"
+	"github.com/icon-project/goloop/common/db"
+	"github.com/icon-project/goloop/module"
 	"log"
 	"math/big"
 	"testing"
-
-	"github.com/icon-project/goloop/common/db"
-	"github.com/icon-project/goloop/module"
 )
 
-const testReceiptNum = 10
-
-var resultReceipt map[int][]byte
-
-func iterReceiptList(receiptList module.ReceiptList) {
-	m := make(map[int]bool)
-	for iter := receiptList.Iterator(); iter.Has(); iter.Next() {
-		if r, err := iter.Get(); err == nil {
-			if rImple, ok := r.(*receipt); ok {
-				if rImple.cumulativeStepUsed.Cmp(&rImple.stepPrice) != 0 {
-					panic("FAILED")
-				} else if rImple.stepPrice.Cmp(&rImple.stepUsed) != 0 {
-					panic("Failed 2")
-				}
-				m[int(rImple.cumulativeStepUsed.Int64())] = true
-			} else {
-				log.Panicf("failed\n")
-			}
-		} else {
-			log.Panicf("err is not nil\n")
-		}
-	}
-
-	for i := 0; i < testReceiptNum; i++ {
-		if m[i] == false {
-			log.Panicf("%d is not received\n", i)
-		}
-	}
-}
-
-func loopGetReceiptList(receiptList module.ReceiptList) {
-	for i := 0; i < testReceiptNum; i++ {
-		if r, err := receiptList.Get(i); err == nil {
-			if bytes.Compare(r.Bytes(), resultReceipt[i]) != 0 {
-				panic("FAILED")
-			}
-		}
-	}
-}
-
 func TestReceiptList(t *testing.T) {
-	resultReceipt = make(map[int][]byte)
-	receiptSlice := make([]*receipt, testReceiptNum)
-	for i := 0; i < testReceiptNum; i++ {
-		receiptSlice[i] =
-			&receipt{cumulativeStepUsed: *big.NewInt(int64(i)),
-				stepUsed:  *big.NewInt(int64(i)),
-				stepPrice: *big.NewInt(int64(i)),
-				eventLogs: nil,
-				success:   i%2 == 0,
-			}
+	rslice := make([]Receipt, 0)
+
+	var used, price big.Int
+
+	addr1 := common.NewAddressFromString("hx8888888888888888888888888888888888888888")
+	for i := 0; i < 5; i++ {
+		r1 := NewReceipt(addr1)
+		used.SetInt64(int64(i * 100))
+		price.SetInt64(int64(i * 10))
+		r1.SetResult(module.StatusNotPayable, &used, &price, nil)
+		rslice = append(rslice, r1)
 	}
+	addr2 := common.NewAddressFromString("cx0003737589788888888888888888888888888888")
+	for i := 0; i < 5; i++ {
+		r2 := NewReceipt(addr2)
+		used.SetInt64(int64(i * 100))
+		price.SetInt64(int64(i * 10))
+		r2.SetResult(module.StatusNotPayable, &used, &price, nil)
+		rslice = append(rslice, r2)
+	}
+
 	mdb := db.NewMapDB()
 
-	interfaceReceipts := make([]Receipt, testReceiptNum)
-	for i, v := range receiptSlice {
-		interfaceReceipts[i] = v
-		resultReceipt[i] = v.Bytes()
-	}
-	receiptListFromSlice := NewReceiptListFromSlice(mdb, interfaceReceipts)
-	iterReceiptList(receiptListFromSlice)
-	loopGetReceiptList(receiptListFromSlice)
-	hash := receiptListFromSlice.Hash()
-	receiptListFromSlice.Flush()
+	rl1 := NewReceiptListFromSlice(mdb, rslice)
+	hash := rl1.Hash()
+	rl1.Flush()
 
-	receiptListFromHash := NewReceiptListFromHash(mdb, hash)
-	loopGetReceiptList(receiptListFromHash)
-	iterReceiptList(receiptListFromHash)
+	log.Printf("Hash for receipts : <%x>", hash)
+
+	rl2 := NewReceiptListFromHash(mdb, hash)
+	idx := 0
+	for itr := rl2.Iterator(); itr.Has(); itr.Next() {
+		r, err := itr.Get()
+		if err != nil {
+			t.Errorf("Fail on Get Receipt from ReceiptList from Hash err=%+v", err)
+		}
+		if !bytes.Equal(rslice[idx].Bytes(), r.Bytes()) {
+			t.Errorf("Fail on comparing bytes for Receipt[%d]", idx)
+		}
+		if err := rslice[idx].Check(r); err != nil {
+			t.Errorf("Fail on Check Receipt[%d] err=%+v", idx, err)
+		}
+		idx++
+	}
 }
