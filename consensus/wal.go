8e83dcc63c206ba5fd8936ec966fa9b1d25248ee
Hyunil Jung <hijung@icon.foundation>
2019-01-17 11:15:10 +0900

    Implemented write ahead log.

---
+++ consensus/wal.go
@@ -0,0 +1,403 @@
+package consensus
+
+import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
+	"hash/crc32"
+	"io"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/icon-project/goloop/common/codec"
+	"github.com/pkg/errors"
+)
+
+const (
+	configWALBufSize              = 1024 * 1
+	configWALFileLimit            = 1024 * 1024 * 2
+	configWALTotalLimit           = configWALFileLimit * 4
+	configWALHousekeepingInterval = time.Second * 1
+)
+
+const (
+	maxUint64        uint64      = ^uint64(0)
+	walPermission    os.FileMode = 0600
+	walDirPermission os.FileMode = 0700
+	headerLen                    = 8
+)
+
+var crc32c = crc32.MakeTable(crc32.Castagnoli)
+
+type WALWriter interface {
+	Write(v interface{}) error
+	WriteBytes([]byte) (int, error)
+	Sync() error
+	Shift() error
+	Close() error
+}
+
+type WALReader interface {
+	Read(v interface{}) (left []byte, err error)
+	ReadBytes() ([]byte, error)
+	Close() error
+}
+
+type WALConfig struct {
+	FileLimit            int64
+	TotalLimit           int64
+	HousekeepingInterval time.Duration
+}
+
+type file struct {
+	*os.File
+}
+
+type walWriter struct {
+	mutex   sync.Mutex
+	id      string
+	cfg     WALConfig
+	buf     *bufio.Writer
+	tail    file
+	tailIdx uint64
+
+	ticker     *time.Ticker
+	tickerStop chan struct{}
+}
+
+type walInfo struct {
+	headIdx   uint64
+	tailIdx   uint64
+	totalSize int64
+	tailSize  int64
+}
+
+func readWALInfo(id string) (*walInfo, error) {
+	groupDir := filepath.Dir(id)
+	var minIndex, maxIndex uint64 = maxUint64, 0
+	var totalSize, tailSize int64 = 0, 0
+
+	dir, err := os.Open(groupDir)
+	if err != nil {
+		return nil, errors.Wrapf(os.ErrNotExist, "no dir %v for wal %v", groupDir, id)
+	}
+	defer dir.Close()
+	entries, err := dir.Readdir(0)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	prefix := filepath.Base(id) + "_"
+	for _, entry := range entries {
+		if !strings.HasPrefix(entry.Name(), prefix) {
+			continue
+		}
+		fileSize := entry.Size()
+		totalSize += fileSize
+		idxStr := entry.Name()[len(prefix):]
+		idx, err := strconv.ParseUint(idxStr, 10, 64)
+		if err != nil {
+			continue
+		}
+		//log.Printf("file=%v size=%v\n", entry.Name(), entry.Size())
+		if maxIndex == 0 || maxIndex < idx {
+			maxIndex = idx
+			tailSize = fileSize
+		}
+		if idx < minIndex {
+			minIndex = idx
+		}
+	}
+
+	return &walInfo{
+		minIndex,
+		maxIndex,
+		totalSize,
+		tailSize,
+	}, nil
+}
+
+// id is in the form /wal/dir/prefix
+func OpenWALForWrite(id string, cfg *WALConfig) (WALWriter, error) {
+	w := &walWriter{
+		id:  id,
+		cfg: *cfg,
+	}
+	if w.cfg.FileLimit <= 0 {
+		w.cfg.FileLimit = configWALFileLimit
+	}
+	if w.cfg.TotalLimit <= 0 {
+		w.cfg.TotalLimit = configWALTotalLimit
+	}
+	if w.cfg.HousekeepingInterval <= time.Duration(0) {
+		w.cfg.HousekeepingInterval = configWALHousekeepingInterval
+	}
+
+	w.buf = bufio.NewWriterSize(&w.tail, configWALBufSize)
+	wi, err := readWALInfo(w.id)
+	if IsNotExist(err) {
+		if err = os.MkdirAll(filepath.Dir(id), walDirPermission); err != nil {
+			return nil, errors.WithStack(err)
+		}
+		wi, err = readWALInfo(w.id)
+	}
+	if err != nil {
+		return nil, err
+	}
+	w.tail.File, err = os.OpenFile(fmt.Sprintf("%s_%d", w.id, wi.tailIdx), os.O_CREATE|os.O_WRONLY|os.O_APPEND, walPermission)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	w.tailIdx = wi.tailIdx
+
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+
+	w.startHousekeeping()
+
+	return w, nil
+}
+
+func (w *walWriter) WriteBytes(payload []byte) (int, error) {
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+
+	crc := crc32.Checksum(payload, crc32c)
+	payloadLen := len(payload)
+	frameLen := headerLen + payloadLen
+
+	frame := make([]byte, frameLen)
+	binary.BigEndian.PutUint32(frame[0:4], crc)
+	binary.BigEndian.PutUint32(frame[4:headerLen], uint32(payloadLen))
+	copy(frame[headerLen:], payload)
+	//log.Printf("wal write crc=%x payloadLen:%v payload:%x\n", crc, payloadLen, payload)
+
+	return w.buf.Write(frame)
+}
+
+func (w *walWriter) Write(v interface{}) error {
+	bs, err := codec.MarshalToBytes(v)
+	if err != nil {
+		return err
+	}
+	_, err = w.WriteBytes(bs)
+	return err
+}
+
+func (w *walWriter) Sync() error {
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+
+	return w.sync()
+}
+
+func (w *walWriter) sync() error {
+	err := w.buf.Flush()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return w.tail.Sync()
+}
+
+func (w *walWriter) startHousekeeping() {
+	w.ticker = time.NewTicker(w.cfg.HousekeepingInterval)
+	w.tickerStop = make(chan struct{})
+	go w.housekeep(w.ticker.C, w.tickerStop)
+}
+
+func (w *walWriter) stopHousekeeping() {
+	w.ticker.Stop()
+	w.tickerStop <- struct{}{}
+}
+
+func (w *walWriter) Close() error {
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+
+	w.stopHousekeeping()
+
+	err := w.sync()
+	if err != nil {
+		return err
+	}
+	err = w.tail.File.Close()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+func (w *walWriter) housekeep(t <-chan time.Time, s <-chan struct{}) {
+	for {
+		select {
+		case <-t:
+			w.doHousekeeping()
+		case <-s:
+			break
+		}
+	}
+}
+
+func (w *walWriter) Shift() error {
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+
+	return w.shift()
+}
+
+func (w *walWriter) shift() error {
+	err := w.sync()
+	if err != nil {
+		return err
+	}
+	err = w.tail.File.Close()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	w.tail.File, err = os.OpenFile(fmt.Sprintf("%s_%d", w.id, w.tailIdx+1), os.O_CREATE|os.O_WRONLY|os.O_APPEND, walPermission)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	w.tailIdx++
+	return nil
+}
+
+func (w *walWriter) doHousekeeping() {
+	wi, err := readWALInfo(w.id)
+	if err != nil {
+		panic(err)
+	}
+
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+
+	//log.Printf("wi.tailSize=%v cfg.FileLimit=%v\n", wi.tailSize, w.cfg.FileLimit)
+	if wi.tailSize > w.cfg.FileLimit {
+		err := w.shift()
+		if err != nil {
+			panic(err)
+		}
+	}
+	//log.Printf("wi.totalSize=%v cfg.TotalLimit=%v\n", wi.totalSize, w.cfg.TotalLimit)
+	for wi.totalSize > w.cfg.TotalLimit {
+		path := fmt.Sprintf("%s_%d", w.id, wi.headIdx)
+		fInfo, err := os.Stat(path)
+		if err != nil {
+			err = errors.WithStack(err)
+			panic(err)
+		}
+		err = os.Remove(path)
+		if err != nil {
+			err = errors.WithStack(err)
+			panic(err)
+		}
+		wi.totalSize -= fInfo.Size()
+		wi.headIdx++
+	}
+}
+
+type walReader struct {
+	mutex  sync.Mutex
+	files  []*os.File
+	reader io.Reader
+}
+
+func OpenWALForRead(id string) (WALReader, error) {
+	wi, err := readWALInfo(id)
+	if err != nil {
+		return nil, err
+	}
+	if wi.headIdx > wi.tailIdx {
+		return nil, errors.Wrapf(os.ErrNotExist, "no file for wal %v", id)
+	}
+	files := make([]*os.File, wi.tailIdx-wi.headIdx+1)
+	readers := make([]io.Reader, len(files))
+
+	defer func() {
+		for _, f := range files {
+			if f != nil {
+				f.Close()
+			}
+		}
+	}()
+
+	for i := 0; i < len(files); i++ {
+		files[i], err = os.Open(fmt.Sprintf("%s_%d", id, wi.headIdx+uint64(i)))
+		if err != nil {
+			return nil, errors.WithStack(err)
+		}
+		readers[i] = files[i]
+	}
+	w := &walReader{}
+	w.reader = bufio.NewReaderSize(io.MultiReader(readers...), configWALBufSize)
+	w.files = files
+	files = nil
+	return w, nil
+}
+
+func (w *walReader) ReadBytes() ([]byte, error) {
+	header := make([]byte, headerLen)
+	_, err := io.ReadAtLeast(w.reader, header, headerLen)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	crc := binary.BigEndian.Uint32(header[0:4])
+	payloadLen := binary.BigEndian.Uint32(header[4:headerLen])
+	payload := make([]byte, payloadLen)
+	_, err = io.ReadAtLeast(w.reader, payload, int(payloadLen))
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	actualCRC := crc32.Checksum(payload, crc32c)
+	//log.Printf("wal read crc=%x ccrc=%x payloadLen:%v payload:%x\n", crc, actualCRC, payloadLen, payload)
+	if actualCRC != crc {
+		return nil, errors.Wrapf(errCorruptedWAL, "bad crc: read:%x actural:%x payloadLen:%v payload:%x", crc, actualCRC, payloadLen, payload)
+	}
+
+	return payload, nil
+}
+
+func (w *walReader) Read(v interface{}) ([]byte, error) {
+	bs, err := w.ReadBytes()
+	if err != nil {
+		return nil, err
+	}
+	return codec.UnmarshalFromBytes(bs, v)
+}
+
+func (w *walReader) Close() error {
+	for _, f := range w.files {
+		err := f.Close()
+		if err != nil {
+			return errors.WithStack(err)
+		}
+	}
+	return nil
+}
+
+func IsCorruptedWAL(err error) bool {
+	return errors.Cause(err) == errCorruptedWAL
+}
+
+var errCorruptedWAL = errors.New("errCorruptedWAL")
+
+func IsNotExist(err error) bool {
+	return errors.Cause(err) == os.ErrNotExist
+}
+
+func IsEOF(err error) bool {
+	return errors.Cause(err) == io.EOF
+}
+
+func IsUnexpectedEOF(err error) bool {
+	return errors.Cause(err) == io.ErrUnexpectedEOF
+}
+
+func IsAnyEOF(err error) bool {
+	return IsEOF(err) || IsUnexpectedEOF(err)
+}
