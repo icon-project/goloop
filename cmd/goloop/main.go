5fb7e0133fc6ea4eb1acb5ad5e80359f30e9d387
Jeongseo Park <js.park@icon.foundation>
2019-04-25 18:03:42 +0900

    support Environment variable for CLI with viper, and CLI markdown document

--- cmd/goloop/main.go
+++ cmd/goloop/main.go
@@ -8,17 +8,19 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"path/filepath"
+	"reflect"
 	"runtime/pprof"
+	"strings"
 	"sync/atomic"
 	"syscall"
 
+	"github.com/mitchellh/mapstructure"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
 	"github.com/icon-project/goloop/common"
 	"github.com/icon-project/goloop/common/crypto"
 	"github.com/icon-project/goloop/common/wallet"
-	"github.com/icon-project/goloop/module"
 )
 
 const (
@@ -28,27 +30,12 @@ const (
 type GoLoopConfig struct {
 	NodeConfig
 
-	Key          []byte          `json:"key,omitempty"`
 	KeyStoreData json.RawMessage `json:"key_store"`
-	KeyStorePass string          `json:"key_password"`
-}
+	Key          []byte          `json:"key,omitempty"`
 
-func (config *GoLoopConfig) String() string {
-	return ""
-}
+	KeyStorePass string `json:"key_password"`
 
-func (config *GoLoopConfig) Type() string {
-	return "GoLoopConfig"
-}
-
-func (config *GoLoopConfig) Set(name string) error {
-	config.FilePath, _ = filepath.Abs(name)
-	if bs, e := ioutil.ReadFile(name); e == nil {
-		if err := json.Unmarshal(bs, config); err != nil {
-			return err
-		}
-	}
-	return nil
+	priK *crypto.PrivateKey
 }
 
 var memProfileCnt int32 = 0
@@ -57,79 +44,85 @@ var (
 	version = "unknown"
 	build   = "unknown"
 
-	cfg                          GoLoopConfig
-	keyStoreFile, keyStoreSecret string
-	saveKeyStore                 string
-	nodeDir string
-	cliSocket, eeSocket string
+	cfg                              GoLoopConfig
+	flagCfg                          GoLoopConfig
+	keyStoreFile, keyStoreSecret     string
+	saveKeyStore, saveKeyStoreSecret string
+	nodeDir                          string
+	cliSocket, eeSocket              string
 
 	cpuProfile, memProfile string
 
-	w module.Wallet
+	//viper resolving : flag -> env -> config -> viper.default -> flag.default
+	vc = viper.New()
+
+	viperDecodeOpt = func(c *mapstructure.DecoderConfig) {
+		c.TagName = "json"
+		c.DecodeHook = mapstructure.ComposeDecodeHookFunc(
+			StringInterfaceMapToJsonRawMessageHookFunc,
+			c.DecodeHook)
+	}
 )
 
-func initConfig() {
-	if len(keyStoreFile) > 0 {
-		if ks, err := ioutil.ReadFile(keyStoreFile); err != nil {
-			log.Panicf("Fail to open KeyStore file=%s err=%+v", keyStoreFile, err)
-		} else {
-			cfg.KeyStoreData = ks
-			cfg.Key = []byte{}
+func StringInterfaceMapToJsonRawMessageHookFunc(
+	inputValType reflect.Type, outValType reflect.Type, input interface{}) (interface{}, error) {
+	if outValType.Name() == "RawMessage" {
+		if inputValType.Kind() == reflect.Map && inputValType.Key().Kind() == reflect.String {
+			return json.Marshal(input)
+		} else if inputValType.Kind() == reflect.String && input != "" {
+			return ioutil.ReadFile(input.(string))
 		}
 	}
+	return input, nil
+}
 
-	keyStorePass := []byte(cfg.KeyStorePass)
-	if len(keyStoreSecret) > 0 {
-		if ks, err := ioutil.ReadFile(keyStoreSecret); err != nil {
+func initConfig() {
+	if cfg.FilePath != "" {
+		f, err := os.Open(cfg.FilePath)
+		if err != nil {
+			log.Panicf("Fail to open config file=%s err=%+v", cfg.FilePath, err)
+		}
+		vc.SetConfigType("json")
+		err = vc.ReadConfig(f)
+		if err != nil {
+			log.Panicf("Fail to read config file=%s err=%+v", cfg.FilePath, err)
+		}
+	}
+	err := vc.Unmarshal(&cfg, viperDecodeOpt)
+	if err != nil {
+		log.Panicf("Fail to unmarshall config from env err=%+v", err)
+	}
+	err = vc.Unmarshal(&cfg.NodeConfig, viperDecodeOpt)
+	if err != nil {
+		log.Panicf("Fail to unmarshall config from env err=%+v", err)
+	}
+
+	//[TBD] flag.KeyStoreSecret -> env.KeyStoreSecret ->flag.KeyStorePass -> env.KeyStorePass -> config.KeyStorePass
+	keyStoreSecret = vc.GetString("key_secret")
+	if keyStoreSecret != "" {
+		if ksp, err := ioutil.ReadFile(keyStoreSecret); err != nil {
 			log.Panicf("Fail to open KeySecret file=%s err=%+v", keyStoreSecret, err)
 		} else {
-			keyStorePass = ks
+			cfg.KeyStorePass = strings.TrimSpace(string(ksp))
 		}
-	}
-
-	var priK *crypto.PrivateKey
-	if len(cfg.Key) > 0 {
-		var err error
-		if priK, err = crypto.ParsePrivateKey(cfg.Key); err != nil {
-			log.Panicf("Illegal key data=[%x]", cfg.Key)
-		}
-		cfg.Key = nil
 	}
 
 	if len(cfg.KeyStoreData) > 0 {
-		var err error
-		if len(keyStorePass) == 0 {
+		if len(cfg.KeyStorePass) == 0 {
 			log.Panicf("There is no password information for the KeyStore")
 		}
-		priK, err = wallet.DecryptKeyStore(cfg.KeyStoreData, keyStorePass)
-		if err != nil {
+		if k, err := wallet.DecryptKeyStore(cfg.KeyStoreData, []byte(cfg.KeyStorePass)); err != nil {
 			log.Panicf("Fail to decrypt KeyStore err=%+v", err)
-		}
-	} else {
-		// make sure that cfg.KeyStoreData always has valid value to let them
-		// be stored with -save_key_store option even though the key is
-		// provided by cfg.Key value.
-		if priK == nil {
-			priK, _ = crypto.GenerateKeyPair()
-		}
-		if len(keyStorePass) == 0 {
-			cfg.KeyStorePass = DefaultKeyStorePass
-			keyStorePass = []byte(cfg.KeyStorePass)
-		}
-
-		if ks, err := wallet.EncryptKeyAsKeyStore(priK, keyStorePass); err != nil {
-			log.Panicf("Fail to encrypt private key err=%+v", err)
 		} else {
-			log.Println("Generated KeyStore", common.NewAccountAddressFromPublicKey(priK.PublicKey()).String())
-			cfg.KeyStoreData = ks
+			cfg.priK = k
+		}
+	} else if len(cfg.Key) > 0 {
+		if k, err := crypto.ParsePrivateKey(cfg.Key); err != nil {
+			log.Panicf("Illegal key data=[%x]", cfg.Key)
+		} else {
+			cfg.priK = k
 		}
 	}
-	w, _ = wallet.NewFromPrivateKey(priK)
-
-
-	log.SetFlags(log.Lshortfile | log.Lmicroseconds)
-	prefix := fmt.Sprintf("%x|--|", w.Address().ID()[0:2])
-	log.SetPrefix(prefix)
 
 	if nodeDir != "" {
 		cfg.BaseDir = cfg.ResolveRelative(nodeDir)
@@ -178,33 +171,58 @@ func initConfig() {
 }
 
 func main() {
+	vc.SetEnvPrefix("GOLOOP")
+	vc.AutomaticEnv()
+
 	cobra.OnInitialize(initConfig)
 	rootCmd := &cobra.Command{Use: "goloop"}
 	rootPFlags := rootCmd.PersistentFlags()
-	rootPFlags.VarP(&cfg, "config", "c", "Parsing configuration file")
+	rootPFlags.StringVarP(&cfg.FilePath, "config", "c", "", "Parsing configuration file")
 	rootPFlags.StringVarP(&cliSocket, "node_sock", "s", "",
-		"Node Command Line Interface socket path(default $GOLOOP_SOCK=[node_dir]/cli.sock)")
+		"Node Command Line Interface socket path(default:[node_dir]/cli.sock)")
 	rootPFlags.StringVar(&cpuProfile, "cpuprofile", "", "CPU Profiling data file")
 	rootPFlags.StringVar(&memProfile, "memprofile", "", "Memory Profiling data file")
+	vc.BindPFlags(rootPFlags)
 
 	serverCmd := &cobra.Command{Use: "server", Short: "Server management"}
 	serverFlags := serverCmd.PersistentFlags()
-	serverFlags.StringVar(&cfg.P2PAddr, "p2p", "127.0.0.1:8080", "Advertise ip-port of P2P")
-	serverFlags.StringVar(&cfg.P2PListenAddr, "p2p_listen", "", "Listen ip-port of P2P")
-	serverFlags.StringVar(&cfg.RPCAddr, "rpc", ":9080", "Listen ip-port of JSON-RPC")
+	serverFlags.StringVar(&flagCfg.P2PAddr, "p2p", "127.0.0.1:8080", "Advertise ip-port of P2P")
+	serverFlags.StringVar(&flagCfg.P2PListenAddr, "p2p_listen", "", "Listen ip-port of P2P")
+	serverFlags.StringVar(&flagCfg.RPCAddr, "rpc", ":9080", "Listen ip-port of JSON-RPC")
 	serverFlags.StringVar(&eeSocket, "ee_socket", "", "Execution engine socket path")
 	serverFlags.StringVar(&keyStoreFile, "key_store", "", "KeyStore file for wallet")
 	serverFlags.StringVar(&keyStoreSecret, "key_secret", "", "Secret(password) file for KeyStore")
-	serverFlags.StringVar(&cfg.KeyStorePass, "key_password", "", "Password for the KeyStore file")
-	serverFlags.IntVar(&cfg.EEInstances, "ee_instances", 1, "Number of execution engines")
+	serverFlags.StringVar(&flagCfg.KeyStorePass, "key_password", "", "Password for the KeyStore file")
+	serverFlags.IntVar(&flagCfg.EEInstances, "ee_instances", 1, "Number of execution engines")
 	serverFlags.StringVar(&nodeDir, "node_dir", "",
 		"Node data directory(default:<configuration file path>/<address>)")
+	vc.BindPFlags(serverFlags)
 
 	saveCmd := &cobra.Command{
 		Use:   "save [file]",
 		Short: "Save configuration",
 		Args:  cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
+			if err := makeSureKeyStore(&cfg); err != nil {
+				log.Panic(err)
+			}
+			if saveKeyStore != "" {
+				ks := bytes.NewBuffer(nil)
+				if err := json.Indent(ks, cfg.KeyStoreData, "", "  "); err != nil {
+					log.Panicf("Fail to indenting key data err=%+v", err)
+				}
+				if err := ioutil.WriteFile(saveKeyStore, ks.Bytes(), 0700); err != nil {
+					log.Panicf("Fail to save key store to the file=%s err=%+v", saveKeyStore, err)
+				}
+				cfg.KeyStoreData = nil
+			}
+			if saveKeyStoreSecret != "" {
+				if err := ioutil.WriteFile(saveKeyStoreSecret, []byte(cfg.KeyStorePass), 0700); err != nil {
+					log.Panicf("Fail to save key store to the file=%s err=%+v", saveKeyStore, err)
+				}
+				cfg.KeyStorePass = ""
+			}
+
 			saveFilePath := args[0]
 			f, err := os.OpenFile(saveFilePath,
 				os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0755)
@@ -217,19 +235,11 @@ func main() {
 				log.Panicf("Fail to generate JSON for %+v", cfg)
 			}
 			f.Close()
-
-			if len(saveKeyStore) > 0 {
-				ks := bytes.NewBuffer(nil)
-				if err := json.Indent(ks, cfg.KeyStoreData, "", "  "); err != nil {
-					log.Panicf("Fail to indenting key data err=%+v", err)
-				}
-				if err := ioutil.WriteFile(saveKeyStore, ks.Bytes(), 0700); err != nil {
-					log.Panicf("Fail to save key store to the file=%s err=%+v", saveKeyStore, err)
-				}
-			}
 		},
 	}
-	saveCmd.Flags().StringVar(&saveKeyStore, "save_key_store", "", "File path for storing current KeyStore")
+	saveCmd.Flags().StringVar(&saveKeyStore, "save_key_store", "", "KeyStore File path to save")
+	saveCmd.Flags().StringVar(&saveKeyStoreSecret, "save_key_secret", "", "Secret File path to save")
+
 	serverCmd.AddCommand(saveCmd)
 
 	startCmd := &cobra.Command{
@@ -250,9 +260,18 @@ func main() {
 		log.Printf("Version : %s", version)
 		log.Printf("Build   : %s", build)
 
+		if err := makeSureKeyStore(&cfg); err != nil {
+			log.Panic(err)
+		}
 
+		w, err := wallet.NewFromPrivateKey(cfg.priK)
+		if err != nil {
+			log.Panicf("Fail to create wallet err=%+v", err)
+		}
 
-
+		log.SetFlags(log.Lshortfile | log.Lmicroseconds)
+		prefix := fmt.Sprintf("%x|--|", w.Address().ID()[0:2])
+		log.SetPrefix(prefix)
 
 		n := NewNode(w, &cfg.NodeConfig)
 		n.Start()
@@ -263,5 +282,28 @@ func main() {
 	systemCmd := NewSystemCmd(&cfg)
 	rootCmd.AddCommand(serverCmd, chainCmd, systemCmd)
 
+	genMdCmd := NewGenerateMarkdownCommand()
+	genMdCmd.Hidden = true
+	rootCmd.AddCommand(genMdCmd)
 	rootCmd.Execute()
+}
+
+// make sure that cfg.KeyStoreData always has valid value to let them
+// be stored with --save_key_store option even though the key is
+// provided by cfg.Key value.
+func makeSureKeyStore(cfg *GoLoopConfig) error {
+	if cfg.priK == nil {
+		cfg.priK, _ = crypto.GenerateKeyPair()
+		log.Println("Generated KeyPair", common.NewAccountAddressFromPublicKey(cfg.priK.PublicKey()).String())
+	}
+	if len(cfg.KeyStorePass) == 0 {
+		cfg.KeyStorePass = DefaultKeyStorePass
+	}
+
+	if ks, err := wallet.EncryptKeyAsKeyStore(cfg.priK, []byte(cfg.KeyStorePass)); err != nil {
+		return fmt.Errorf("fail to encrypt private key err=%+v", err)
+	} else {
+		cfg.KeyStoreData = ks
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -14,9 +14,11 @@ require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/labstack/echo/v4 v4.0.0
 	github.com/leodido/go-urn v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.1.2
 	github.com/pkg/errors v0.8.1
 	github.com/spf13/cobra v0.0.3
-	github.com/spf13/pflag v1.0.3 // indirect
+	github.com/spf13/pflag v1.0.3
+	github.com/spf13/viper v1.3.2
 	github.com/stretchr/testify v1.3.0
 	github.com/syndtr/goleveldb v1.0.0
 	github.com/ugorji/go/codec v0.0.0-20181209151446-772ced7fd4c2
