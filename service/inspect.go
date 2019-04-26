f891e1bdec30904bf6b0f7d66a82c284b9b06584
Jeongseo Park <js.park@icon.foundation>
2019-04-26 15:44:59 +0900

    missing file

---
+++ service/inspect.go
@@ -0,0 +1,20 @@
+package service
+
+import (
+	"github.com/icon-project/goloop/module"
+)
+
+func Inspect(c module.Chain) map[string]interface{} {
+	mgr := c.ServiceManager().(*manager)
+	m := make(map[string]interface{})
+	m["normalTxPool"] = inspectTxPool(mgr.normalTxPool)
+	m["patchTxPool"] = inspectTxPool(mgr.patchTxPool)
+	return m
+}
+
+func inspectTxPool(p *TransactionPool) map[string]interface{} {
+	m := make(map[string]interface{})
+	m["size"] = configTxPoolSize
+	m["used"] = p.list.Len()
+	return m
+}
