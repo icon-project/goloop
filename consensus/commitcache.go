edab6bb1223f195b8b1d6206b895dfb5379fdc06
Hyunil Jung <hijung@icon.foundation>
2019-05-27 11:00:30 +0900

    Add consensus.commitCache

---
+++ consensus/commitcache.go
@@ -0,0 +1,48 @@
+package consensus
+
+import (
+	"container/list"
+)
+
+type commit struct {
+	height       int64
+	commitVotes  *commitVoteList
+	votes        *voteList
+	blockPartSet PartSet
+}
+
+type commitCache struct {
+	cap       int
+	heightMap map[int64]*list.Element
+	mru       *list.List
+}
+
+func newCommitCache(cap int) *commitCache {
+	return &commitCache{
+		cap:       cap,
+		heightMap: make(map[int64]*list.Element),
+		mru:       list.New(),
+	}
+}
+
+func (c *commitCache) Put(cm *commit) {
+	if c.mru.Len() == c.cap {
+		cm := c.mru.Remove(c.mru.Back()).(*commit)
+		delete(c.heightMap, cm.height)
+	}
+	e := c.mru.PushFront(cm)
+	c.heightMap[cm.height] = e
+}
+
+func (c *commitCache) GetByHeight(h int64) *commit {
+	if e, ok := c.heightMap[h]; ok {
+		c.mru.MoveToFront(e)
+		return e.Value.(*commit)
+	}
+	return nil
+}
+
+// for test
+func (c *commitCache) _getMRU() *list.List {
+	return c.mru
+}
---
+++ consensus/commitcache_test.go
@@ -0,0 +1,34 @@
+package consensus
+
+import (
+	"container/list"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func assertListHeights(t *testing.T, l *list.List, h ...int64) {
+	assert.Equal(t, l.Len(), len(h))
+	i := 0
+	for e := l.Front(); e != nil; e = e.Next() {
+		assert.Equal(t, e.Value.(*commit).height, h[i])
+		i++
+	}
+}
+
+func TestCache_Simple(t *testing.T) {
+	c := newCommitCache(3)
+	c.Put(&commit{height: 0})
+	cm := c.GetByHeight(0)
+	assert.NotNil(t, cm)
+}
+
+func TestCache_Simple2(t *testing.T) {
+	c := newCommitCache(3)
+	c.Put(&commit{height:0})
+	c.Put(&commit{height:1})
+	c.Put(&commit{height:2})
+	c.GetByHeight(0)
+	c.Put(&commit{height:3})
+	assertListHeights(t, c._getMRU(), 3, 0, 2)
+}
