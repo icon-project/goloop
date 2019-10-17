bd28fa5f76bab682b7ead7484db9304aa2e9862a
Hyunil Jung <hijung@icon.foundation>
2019-10-17 11:13:57 +0900

    Add methods on ValueBuffer and implement ValueBuffer

--- rt/src/java/avm/ValueBuffer.java
+++ rt/src/java/avm/ValueBuffer.java
@@ -1,35 +1,130 @@
 package avm;
 
-public class ValueBuffer {
-    public byte toByte() {
+import java.math.BigInteger;
+
+public class ValueBuffer implements Value {
+    public ValueBuffer() {
+    }
+
+    public ValueBuffer(byte v) {
+    }
+
+    public ValueBuffer(short v) {
+    }
+
+    public ValueBuffer(int v) {
+    }
+
+    public ValueBuffer(long v) {
+    }
+
+    public ValueBuffer(float v) {
+    }
+
+    public ValueBuffer(double v) {
+    }
+
+    public ValueBuffer(char v) {
+    }
+
+    public ValueBuffer(boolean v) {
+    }
+
+    public ValueBuffer(BigInteger v) {
+    }
+
+    public ValueBuffer(Address v) {
+    }
+
+    public ValueBuffer(String v) {
+    }
+
+    public ValueBuffer(byte[] v) {
+    }
+
+    public void set(byte v) {
+    }
+
+    public byte asByte() {
         return (byte) 0;
     }
 
-    public short toShort() {
+    public void set(short v) {
+    }
+
+    public short asShort() {
         return (short) 0;
     }
 
-    public int toInt() {
+    public void set(int v) {
+    }
+
+    public int asInt() {
         return 0;
     }
 
-    public long toLong() {
+    public void set(long v) {
+    }
+
+    public long asLong() {
         return 0;
     }
 
-    public float toFloat() {
+    public void set(float v) {
+    }
+
+    public float asFloat() {
         return 0;
     }
 
-    public double toDouble() {
+    public void set(double v) {
+    }
+
+    public double asDouble() {
         return 0;
     }
 
-    public char toChar() {
+    public void set(char v) {
+    }
+
+    public char asChar() {
         return 0;
     }
 
-    public boolean toBoolean() {
+    public void set(boolean v) {
+    }
+
+    public boolean asBoolean() {
         return false;
     }
+
+    public void set(BigInteger v) {
+    }
+
+    public BigInteger asBigInteger() {
+        return null;
+    }
+
+    public void set(Address v) {
+    }
+
+    public Address asAddress() {
+        return null;
+    }
+
+    public void set(String v) {
+    }
+
+    public String asString() {
+        return null;
+    }
+
+    public void set(byte[] v) {
+    }
+
+    public byte[] asByteArray() {
+        return null;
+    }
+
+    // TODO implement hashCode and equals
 }
--- rt/src/java/p/avm/ValueBuffer.java
+++ rt/src/java/p/avm/ValueBuffer.java
@@ -1,70 +1,221 @@
 package p.avm;
 
+import a.ByteArray;
 import s.java.lang.Object;
+import s.java.lang.String;
 
-public class ValueBuffer extends Object {
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+public class ValueBuffer extends Object implements Value {
     private byte[] raw;
 
     public ValueBuffer() {
+        raw = new byte[0];
+    }
+
+    public ValueBuffer(byte v) {
+        set(v);
+    }
+
+    public ValueBuffer(short v) {
+        set(v);
+    }
+
+    public ValueBuffer(int v) {
+        set(v);
+    }
+
+    public ValueBuffer(long v) {
+        set(v);
+    }
+
+    public ValueBuffer(float v) {
+        set(v);
+    }
+
+    public ValueBuffer(double v) {
+        set(v);
+    }
+
+    public ValueBuffer(char v) {
+        set(v);
+    }
+
+    public ValueBuffer(boolean v) {
+        set(v);
+    }
+
+    public ValueBuffer(s.java.math.BigInteger v) {
+        set(v);
+    }
+
+    public ValueBuffer(Address v) {
+        set(v);
+    }
+
+    public ValueBuffer(String v) {
+        set(v);
+    }
+
+    public ValueBuffer(byte[] v) {
+        set(v);
     }
 
     public void set(byte[] raw) {
         this.raw = raw;
     }
 
-    public void setByte(byte v) {
+    public void avm_set(byte v) {
+        set(v);
     }
 
-    public byte avm_toByte() {
-        return (byte) 0;
+    public byte avm_asByte() {
+        return (new BigInteger(raw)).byteValue();
     }
 
-    public void setShort(byte v) {
+    public void avm_set(short v) {
+        set(v);
     }
 
-    public short avm_toShort() {
-        return (short) 0;
+    public short avm_asShort() {
+        return (new BigInteger(raw)).shortValue();
     }
 
-    public void setInt(int v) {
+    public void avm_set(int v) {
+        set(v);
     }
 
-    public int avm_toInt() {
-        return 0;
+    public int avm_asInt() {
+        return asInt();
     }
 
-    public void setLong(long v) {
+    public int asInt() {
+        return (new BigInteger(raw)).intValue();
     }
 
-    public long avm_toLong() {
-        return 0;
+    public void avm_set(long v) {
+        set(v);
     }
 
-    public void setFloat(float v) {
+    public long avm_asLong() {
+        return (new BigInteger(raw)).longValue();
     }
 
-    public float avm_toFloat() {
-        return 0;
+    public void avm_set(float v) {
+        set(v);
     }
 
-    public void setDouble(double v) {
+    public float avm_asFloat() {
+        return ByteBuffer.wrap(raw).getFloat();
     }
 
-    public double avm_toDouble() {
-        return 0;
+    public void avm_set(double v) {
+        set(v);
     }
 
-    public void setChar(char v) {
+    public double avm_asDouble() {
+        return ByteBuffer.wrap(raw).getDouble();
     }
 
-    public char avm_toChar() {
-        return 0;
+    public void avm_set(char v) {
+        set(v);
     }
 
-    public void setBoolean(boolean v) {
+    public char avm_asChar() {
+        return (char) (new BigInteger(raw)).intValue();
     }
 
-    public boolean avm_toBoolean() {
-        return false;
+    public void avm_set(boolean v) {
+        set(v);
+    }
+
+    public boolean avm_asBoolean() {
+        return (new BigInteger(raw)).intValue() != 0;
+    }
+
+    public void avm_set(s.java.math.BigInteger v) {
+        set(v);
+    }
+
+    public s.java.math.BigInteger avm_asBigInteger() {
+        return new s.java.math.BigInteger(new BigInteger(raw));
+    }
+
+    public void avm_set(Address v) {
+        set(v);
+    }
+
+    public Address avm_asAddress() {
+        return new Address(raw);
+    }
+
+    public void avm_set(String v) {
+        set(v);
+    }
+
+    public String avm_asString() {
+        return new String(new java.lang.String(raw, StandardCharsets.UTF_8));
+    }
+
+    public void avm_set(ByteArray v) {
+        set(v);
+    }
+
+    public ByteArray avm_asByteArray() {
+        return new ByteArray(raw.clone());
+    }
+
+    public void set(byte v) {
+        raw = BigInteger.valueOf(v).toByteArray();
+    }
+
+    public void set(short v) {
+        raw = BigInteger.valueOf(v).toByteArray();
+    }
+
+    public void set(int v) {
+        raw = BigInteger.valueOf(v).toByteArray();
+    }
+
+    public void set(long v) {
+        raw = BigInteger.valueOf(v).toByteArray();
+    }
+
+    public void set(float v) {
+        raw = ByteBuffer.allocate(Float.BYTES).putFloat(v).array();
+    }
+
+    public void set(double v) {
+        raw = ByteBuffer.allocate(Double.BYTES).putDouble(v).array();
+    }
+
+    public void set(char v) {
+        raw = BigInteger.valueOf(v).toByteArray();
+    }
+
+    public void set(boolean v) {
+        raw = BigInteger.valueOf(v ? 1 : 0).toByteArray();
+    }
+
+    public void set(s.java.math.BigInteger v) {
+        raw = v.getUnderlying().toByteArray();
+    }
+
+    public void set(Address v) {
+        raw = v.toByteArray();
+    }
+
+    public void set(String v) {
+        raw = v.getUnderlying().getBytes(StandardCharsets.UTF_8);
+    }
+
+    public void set(ByteArray v) {
+        raw = v.getUnderlying().clone();
+    }
+
+    public byte[] asByteArray() {
+        return raw;
     }
 }
