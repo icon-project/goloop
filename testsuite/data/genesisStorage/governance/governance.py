76c046524613deabdc96eb5bb65ea494ce54a2f5
Jaechang Namgoong <jnamgoong@iconloop.com>
2020-01-02 15:16:02 +0900

    Update governance.py
    - Remove incorrect comments
    - Reformat file to remove PEP8 warnings
    - Adopt the predefined ZERO_SCORE_ADDRESS
    - Do not export 'fallback' method

--- testsuite/data/genesisStorage/governance/governance.py
+++ testsuite/data/genesisStorage/governance/governance.py
@@ -3,59 +3,75 @@ from iconservice import *
 TAG = 'Governance'
 
 
-# An interface of token to give a reward to anyone who contributes
 class SystemInterface(InterfaceScore):
     @interface
     def setRevision(self, code: int):
         pass
+
     @interface
     def acceptScore(self, txHash: bytes):
         pass
+
     @interface
     def rejectScore(self, txHash: bytes):
         pass
+
     @interface
     def blockScore(self, address: Address):
         pass
+
     @interface
     def unblockScore(self, address: Address):
         pass
+
     @interface
     def setStepPrice(self, price: int):
         pass
+
     @interface
     def setStepCost(self, type: str, cost: int):
         pass
+
     @interface
     def setMaxStepLimit(self, contextType: str, limit: int):
         pass
+
     @interface
     def addDeployer(self, address: Address):
         pass
+
     @interface
     def removeDeployer(self, address: Address):
         pass
+
     @interface
     def grantValidator(self, address: Address):
         pass
+
     @interface
     def revokeValidator(self, address: Address):
         pass
+
     @interface
     def addMember(self, address: Address):
         pass
+
     @interface
     def removeMember(self, address: Address):
         pass
+
     @interface
     def addLicense(self, contentId: str):
         pass
+
     @interface
     def removeLicense(self, contentId: str):
         pass
+
     @interface
     def setTimestampThreshold(self, threshold: int):
         pass
+
     @interface
     def setRoundLimitFactor(self, factor: int):
         pass
@@ -65,30 +81,22 @@ class Governance(IconScoreBase):
 
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
-        self.system_score = self.create_interface_score(Address.from_string("cx0000000000000000000000000000000000000000"), SystemInterface)
+        self.system_score = self.create_interface_score(ZERO_SCORE_ADDRESS, SystemInterface)
 
-    def on_install(self, name : str, value : int) -> None:
-        print("install governance : ", name, ", value : ", value)
-        """
-        Called when this SCORE first deployed.
-
-        :param _fundingGoalInIcx: The funding goal of this crowdsale, in ICX
-        :param _tokenScore: SCORE address of token that will be used for the rewards
-        :param _durationInBlocks: the sale duration is given in number of blocks
-        """
+    def on_install(self, name: str, value: int) -> None:
+        Logger.info(f'install governance: {name}, value: {value}')
         super().on_install()
-
 
     def on_update(self) -> None:
         super().on_update()
 
-    @payable
-    def fallback(self):
-        """
-        Called when anyone sends funds to the SCORE.
-        This SCORE regards it as a contribution.
-        """
-        Logger.debug(f'FundTransfer({self.msg.sender}, {self.msg.value}, True)', TAG)
+    # NOTE: Governance should not accept incoming funds
+    # @payable
+    # def fallback(self):
+    #     """
+    #     Called when anyone sends funds to the SCORE.
+    #     """
+    #     Logger.debug(f'FundTransfer({self.msg.sender}, {self.msg.value})', TAG)
 
     @external
     def setRevision(self, code: int):
@@ -125,9 +133,11 @@ class Governance(IconScoreBase):
     @external
     def grantValidator(self, address: Address):
         self.system_score.grantValidator(address)
+
     @external
     def revokeValidator(self, address: Address):
         self.system_score.revokeValidator(address)
+
     @external
     def addMember(self, address: Address):
         self.system_score.addMember(address)
@@ -135,6 +145,7 @@ class Governance(IconScoreBase):
     @external
     def removeMember(self, address: Address):
         self.system_score.removeMember(address)
+
     @external
     def addDeployer(self, address: Address):
         self.system_score.addDeployer(address)
