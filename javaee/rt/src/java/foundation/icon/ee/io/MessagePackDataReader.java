d2ba36ba3172541a67725b6f001f7c86c1c4b55c
Hyunil Jung <hijung@iconloop.com>
2020-01-22 10:36:59 +0900

    Implement DataReader/Writer for Message Pack

---
+++ javaee/rt/src/java/foundation/icon/ee/io/MessagePackDataReader.java
@@ -0,0 +1,224 @@
+package foundation.icon.ee.io;
+
+import org.msgpack.core.MessageFormat;
+import org.msgpack.core.MessageInsufficientBufferException;
+import org.msgpack.core.MessagePack;
+import org.msgpack.core.MessageTypeException;
+import org.msgpack.core.MessageUnpacker;
+import org.msgpack.value.ValueType;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class MessagePackDataReader implements DataReader {
+    private static class ListFrame {
+        int current;
+        int length;
+    }
+
+    private MessageUnpacker unpacker;
+    private ArrayList<ListFrame> frames = new ArrayList<>();
+    private ListFrame topFrame;
+
+    public MessagePackDataReader(byte[] data) {
+        this(MessagePack.newDefaultUnpacker(data));
+    }
+
+    public MessagePackDataReader(MessageUnpacker unpacker) {
+        this.unpacker = unpacker;
+        this.topFrame = new ListFrame();
+        this.frames.add(topFrame);
+        this.topFrame.length = Integer.MAX_VALUE;
+    }
+
+    public RuntimeException convert(Exception e) {
+        if (e instanceof MessageTypeException ||
+                e instanceof MessageInsufficientBufferException) {
+            return new IllegalStateException();
+        }
+        return new UnsupportedOperationException();
+    }
+
+    public boolean readBoolean() {
+        try {
+            var res = unpacker.unpackBoolean();
+            topFrame.current++;
+            return res;
+        } catch (Exception e) {
+            throw convert(e);
+        }
+    }
+
+    public byte readByte() {
+        try {
+            var res = unpacker.unpackByte();
+            topFrame.current++;
+            return res;
+        } catch (Exception e) {
+            throw convert(e);
+        }
+    }
+
+    public short readShort() {
+        try {
+            var res = unpacker.unpackShort();
+            topFrame.current++;
+            return res;
+        } catch (Exception e) {
+            throw convert(e);
+        }
+    }
+
+    public char readChar() {
+        try {
+            var res = (char) unpacker.unpackInt();
+            topFrame.current++;
+            return res;
+        } catch (Exception e) {
+            throw convert(e);
+        }
+    }
+
+    public int readInt() {
+        try {
+            var res = unpacker.unpackInt();
+            topFrame.current++;
+            return res;
+        } catch (Exception e) {
+            throw convert(e);
+        }
+    }
+
+    public float readFloat() {
+        try {
+            var res = unpacker.unpackFloat();
+            topFrame.current++;
+            return res;
+        } catch (Exception e) {
+            throw convert(e);
+        }
+    }
+
+    public long readLong() {
+        try {
+            var res = unpacker.unpackLong();
+            topFrame.current++;
+            return res;
+        } catch (Exception e) {
+            throw convert(e);
+        }
+    }
+
+    public double readDouble() {
+        try {
+            var res = unpacker.unpackDouble();
+            topFrame.current++;
+            return res;
+        } catch (Exception e) {
+            throw convert(e);
+        }
+    }
+
+    public BigInteger readBigInteger() {
+        try {
+            var res = unpacker.unpackBigInteger();
+            topFrame.current++;
+            return res;
+        } catch (Exception e) {
+            throw convert(e);
+        }
+    }
+
+    public String readString() {
+        try {
+            var res = unpacker.unpackString();
+            topFrame.current++;
+            return res;
+        } catch (Exception e) {
+            throw convert(e);
+        }
+    }
+
+    public byte[] readByteArray() {
+        try {
+            MessageFormat fmt = unpacker.getNextFormat();
+            if (fmt.getValueType() != ValueType.BINARY) {
+                throw new IllegalStateException();
+            }
+            var l = unpacker.unpackBinaryHeader();
+            var ba = new byte[l];
+            unpacker.readPayload(ba);
+            topFrame.current++;
+            return ba;
+        } catch (Exception e) {
+            throw convert(e);
+        }
+    }
+
+    public boolean readNullity() {
+        return tryReadNull();
+    }
+
+    public void skip(int count) {
+        try {
+            unpacker.skipValue(count);
+            topFrame.current++;
+        } catch (Exception e) {
+            throw convert(e);
+        }
+    }
+
+    private void readContainerHeader(ValueType type) {
+        try {
+            MessageFormat fmt = unpacker.getNextFormat();
+            ValueType vt = fmt.getValueType();
+            int l;
+            if (vt != type) {
+                throw new IllegalStateException();
+            }
+            if (vt == ValueType.ARRAY) {
+                l = unpacker.unpackArrayHeader();
+            } else {
+                assert vt == ValueType.MAP;
+                l = unpacker.unpackMapHeader();
+            }
+            topFrame = new ListFrame();
+            topFrame.length = l;
+            frames.add(topFrame);
+        } catch (Exception e) {
+            throw convert(e);
+        }
+    }
+
+    public void readListHeader() {
+        readContainerHeader(ValueType.ARRAY);
+    }
+
+    public void readMapHeader() {
+        readContainerHeader(ValueType.MAP);
+    }
+
+    public boolean hasNext() {
+        return topFrame.current < topFrame.length;
+    }
+
+    public void readFooter() {
+        frames.remove(frames.size() - 1);
+        topFrame = frames.get(frames.size() - 1);
+        topFrame.current++;
+    }
+
+    public boolean tryReadNull() {
+        try {
+            MessageFormat fmt = unpacker.getNextFormat();
+            if (fmt == MessageFormat.NIL) {
+                unpacker.unpackNil();
+                topFrame.current++;
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            throw convert(e);
+        }
+    }
+}
---
+++ javaee/rt/src/java/foundation/icon/ee/io/MessagePackDataWriter.java
@@ -0,0 +1,148 @@
+package foundation.icon.ee.io;
+
+import i.RuntimeAssertionError;
+import org.msgpack.core.MessageBufferPacker;
+import org.msgpack.core.MessagePack;
+
+import java.io.IOException;
+import java.math.BigInteger;
+
+public class MessagePackDataWriter implements DataWriter {
+    private MessageBufferPacker packer;
+
+    public MessagePackDataWriter() {
+        this.packer = MessagePack.newDefaultBufferPacker();
+    }
+
+    public void write(boolean v) {
+        try {
+            packer.packBoolean(v);
+        } catch (IOException e) {
+            RuntimeAssertionError.unexpected(e);
+        }
+    }
+
+    public void write(byte v) {
+        try {
+            packer.packByte(v);
+        } catch (IOException e) {
+            RuntimeAssertionError.unexpected(e);
+        }
+    }
+
+    public void write(short v) {
+        try {
+            packer.packShort(v);
+        } catch (IOException e) {
+            RuntimeAssertionError.unexpected(e);
+        }
+    }
+
+    public void write(char v) {
+        try {
+            packer.packInt(v);
+        } catch (IOException e) {
+            RuntimeAssertionError.unexpected(e);
+        }
+    }
+
+    public void write(int v) {
+        try {
+            packer.packInt(v);
+        } catch (IOException e) {
+            RuntimeAssertionError.unexpected(e);
+        }
+    }
+
+    public void write(float v) {
+        try {
+            packer.packFloat(v);
+        } catch (IOException e) {
+            RuntimeAssertionError.unexpected(e);
+        }
+    }
+
+    public void write(long v) {
+        try {
+            packer.packLong(v);
+        } catch (IOException e) {
+            RuntimeAssertionError.unexpected(e);
+        }
+    }
+
+    public void write(double v) {
+        try {
+            packer.packDouble(v);
+        } catch (IOException e) {
+            RuntimeAssertionError.unexpected(e);
+        }
+    }
+
+    public void write(BigInteger v) {
+        try {
+            packer.packBigInteger(v);
+        } catch (IOException e) {
+            RuntimeAssertionError.unexpected(e);
+        }
+    }
+
+    public void write(String v) {
+        try {
+            packer.packString(v);
+        } catch (IOException e) {
+            RuntimeAssertionError.unexpected(e);
+        }
+    }
+
+    public void write(byte[] v) {
+        try {
+            packer.packBinaryHeader(v.length);
+            packer.writePayload(v);
+        } catch (IOException e) {
+            RuntimeAssertionError.unexpected(e);
+        }
+    }
+
+    public void writeNullity(boolean nullity) {
+    }
+
+    public void writeListHeader(int l) {
+        try {
+            packer.packArrayHeader(l);
+        } catch (IOException e) {
+            RuntimeAssertionError.unexpected(e);
+        }
+    }
+
+    public void writeMapHeader(int l) {
+        try {
+            packer.packMapHeader(l);
+        } catch (IOException e) {
+            RuntimeAssertionError.unexpected(e);
+        }
+    }
+
+    public void writeFooter() {
+    }
+
+    public void writeNull() {
+        try {
+            packer.packNil();
+        } catch (IOException e) {
+            RuntimeAssertionError.unexpected(e);
+        }
+    }
+
+    public void flush() {
+        try {
+            packer.flush();
+        } catch (IOException e) {
+            RuntimeAssertionError.unexpected(e);
+        }
+    }
+
+    public byte[] toByteArray() {
+        flush();
+        return packer.toByteArray();
+    }
+}
