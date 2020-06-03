384fb863fdf095673839b97c430f93495ab77422
Hyunil Jung <hijung@iconloop.com>
2020-06-03 16:11:08 +0900

    Add deploy test

---
+++ javaee/exec/test/java/foundation/icon/ee/ipc/DeployTest.java
@@ -0,0 +1,55 @@
+package foundation.icon.ee.ipc;
+
+import foundation.icon.ee.test.GoldenTest;
+import foundation.icon.ee.test.TransactionException;
+import foundation.icon.ee.types.Status;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DeployTest extends GoldenTest {
+    public static class NoConstructor {
+    }
+
+    public static class PackagePrivateConstructor {
+        PackagePrivateConstructor() {
+        }
+    }
+
+    public static class ProtectedConstructor {
+        protected ProtectedConstructor() {
+        }
+    }
+
+    public static class PrivateConstructor {
+        private PrivateConstructor() {
+        }
+    }
+
+    public static abstract class Abstract {
+    }
+
+    public interface Interface {
+    }
+
+    @Test
+    public void test() {
+        sm.deploy(NoConstructor.class);
+        var e = assertThrows(TransactionException.class,
+                () -> sm.deploy(PackagePrivateConstructor.class));
+        assertEquals(e.getResult().getStatus(), Status.IllegalFormat);
+        e = assertThrows(TransactionException.class,
+                () -> sm.deploy(ProtectedConstructor.class));
+        assertEquals(e.getResult().getStatus(), Status.IllegalFormat);
+        e = assertThrows(TransactionException.class,
+                () -> sm.deploy(PrivateConstructor.class));
+        assertEquals(e.getResult().getStatus(), Status.IllegalFormat);
+        e = assertThrows(TransactionException.class,
+                () -> sm.deploy(Abstract.class));
+        assertEquals(e.getResult().getStatus(), Status.IllegalFormat);
+        e = assertThrows(TransactionException.class,
+                () -> sm.deploy(Interface.class));
+        assertEquals(e.getResult().getStatus(), Status.IllegalFormat);
+    }
+}
