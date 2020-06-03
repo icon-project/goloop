6fa21554da3e2b04d0de6349258c6ff659c61d88
Hyunil Jung <hijung@iconloop.com>
2020-06-03 16:00:32 +0900

    Add some internal exceptions

---
+++ javaee/rt/src/java/foundation/icon/ee/types/IllegalFormatException.java
@@ -0,0 +1,23 @@
+package foundation.icon.ee.types;
+
+public class IllegalFormatException extends CodedException {
+    public IllegalFormatException() {
+        this(Status.getMessage(Status.IllegalFormat));
+    }
+
+    public IllegalFormatException(String message) {
+        super(message);
+    }
+
+    public IllegalFormatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public IllegalFormatException(Throwable cause) {
+        this(Status.getMessage(Status.IllegalFormat), cause);
+    }
+
+    public int getCode() {
+        return Status.IllegalFormat;
+    }
+}
---
+++ javaee/rt/src/java/foundation/icon/ee/types/UnknownFailureException.java
@@ -0,0 +1,23 @@
+package foundation.icon.ee.types;
+
+public class UnknownFailureException extends CodedException {
+    public UnknownFailureException() {
+        super();
+    }
+
+    public UnknownFailureException(String message) {
+        super(message);
+    }
+
+    public UnknownFailureException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UnknownFailureException(Throwable cause) {
+        super(cause);
+    }
+
+    public int getCode() {
+        return Status.UnknownFailure;
+    }
+}
