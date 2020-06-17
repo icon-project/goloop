5fccdfea7b1b8617ebd6ac6849a983b8af4dc28c
Hyunil Jung <hijung@iconloop.com>
2020-06-17 11:07:16 +0900

    Minor document update

--- javaee/api/src/java/score/Context.java
+++ javaee/api/src/java/score/Context.java
@@ -340,7 +340,9 @@ public final class Context {
     }
 
     /**
-     * Records a log on the blockchain.
+     * Records a log on the blockchain. It is recommended to use
+     * {@link foundation.icon.ee.tooling.abi.EventLog} annotation rather than
+     * this method directly.
      *
      * @param indexed indexed data
      * @param data extra data
