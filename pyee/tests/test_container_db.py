2659c813ee46f12e4a6dbfaf3abb0a9a58425323
MoonKyu Song <mksong@iconloop.com>
2020-08-28 18:11:20 +0900

    Update test cases of test_container_db.py
    Use ContextDatabase to check key validity instead of IconScoreDatabase.
    Add the case, testing inter-change between sub_db and dict.

--- pyee/tests/test_container_db.py
+++ pyee/tests/test_container_db.py
@@ -16,14 +16,15 @@ import os
 import unittest
 
 from pyexec.base.address import Address, AddressPrefix
-from pyexec.database.db import IconScoreDatabase
+from pyexec.database.db import IconScoreDatabase, ContextDatabase
 from pyexec.database.factory import ContextDatabaseFactory
 from pyexec.icon_constant import IconScoreContextType
 from pyexec.iconscore.icon_container_db import (
     ARRAY_DB_ID, DICT_DB_ID, VAR_DB_ID,
-    ArrayDB, DictDB, VarDB, get_encoded_key
+    ArrayDB, DictDB, VarDB, get_encoded_key, ContainerUtil
 )
 from pyexec.iconscore.icon_score_context import ContextContainer, IconScoreContext
+from pyexec.utils import sha3_256
 
 
 class DummyProxy(object):
@@ -49,7 +50,7 @@ class TestContainerDB(unittest.TestCase):
     def get_score_db():
         address = Address.from_data(AddressPrefix.EOA, os.urandom(20))
         context_db = ContextDatabaseFactory.create_by_address(address)
-        return IconScoreDatabase(address, context_db)
+        return context_db, IconScoreDatabase(address, context_db)
 
     def setUp(self):
         ContextDatabaseFactory.open(DummyProxy(), ContextDatabaseFactory.Mode.SINGLE_DB)
@@ -57,95 +58,94 @@ class TestContainerDB(unittest.TestCase):
         ContextContainer._push_context(context)
 
     def test_dict_db(self):
-        db = self.get_score_db()
-        for c in self._dict_cases:
-            _subs = c[1]
-            _depth = len(_subs)
-            _dict = DictDB(c[0], db, value_type=bytes, depth=_depth)
-            for i in range(_depth - 1):
-                _dict = _dict[_subs[i]]
-            _dict[_subs[-1]] = c[2]
+        cdb, db = self.get_score_db()
+        ctx = db._context
+        cases = [
+            ['name1', ['s1'], 'k1', b'<v1>', [DICT_DB_ID, 'name1', 's1', 'k1']],
+            ['name2', [], 'k1', b'<v2>', [DICT_DB_ID, 'name2', 'k1']],
+        ]
+        for c in cases:
+            keys = c[1]
+            ddb = db
+            for i in range(len(keys)):
+                ddb = ddb.get_sub_db(keys[i])
 
-            _prefix = DICT_DB_ID + get_encoded_key(c[0])
-            for i in range(_depth):
-                _prefix += get_encoded_key(_subs[i])
-            value = db.get(_prefix)
-            self.assertEqual(c[2], value)
+            dbase = DictDB(c[0], ddb, value_type=bytes)
+            dbase[c[2]] = c[3]
+
+            key = b''.join(map(lambda x: get_encoded_key(x), c[4]))
+            self.assertEqual(c[3], cdb.get(ctx, sha3_256(key)))
 
     def test_array_db(self):
-        db = self.get_score_db()
+        cdb, db = self.get_score_db()
+        ctx = db._context
         cases = [
-            ['<name>', [b'v0']],
-            ['<name2>', [b'v1', b'v2']],
-            ['<name3>', [b'<value1>', b'<value2>', b'<value3>']]
+            ['name1', ['s1'], [b'<v1>'], [ARRAY_DB_ID, 'name1', 's1']],
+            ['name2', [], [b'<v2>', b'<v3>'], [ARRAY_DB_ID, 'name2']],
         ]
         for c in cases:
-            _subs = c[1]
-            _array = ArrayDB(c[0], db, value_type=bytes)
-            for i in range(len(_subs)):
-                _array.put(_subs[i])
-            self.assertEqual(len(_subs), len(_array))
+            keys = c[1]
+            ddb = db
+            for i in range(len(keys)):
+                ddb = ddb.get_sub_db(keys[i])
 
-            _prefix = ARRAY_DB_ID + get_encoded_key(c[0])
-            values = []
-            for i in range(len(_subs)):
-                values.append(db.get(_prefix + get_encoded_key(i)))
-            self.assertEqual(_subs, values)
+            values = c[2]
+            dbase = ArrayDB(c[0], ddb, value_type=bytes)
+            for v in values:
+                dbase.put(v)
+
+            key = b''.join(map(lambda x: get_encoded_key(x), c[3]))
+            self.assertEqual(ContainerUtil.encode_value(len(values)), cdb.get(ctx, sha3_256(key)))
+            for i in range(len(values)):
+                key2 = key+get_encoded_key(i)
+                self.assertEqual(values[i], cdb.get(ctx, sha3_256(key2)))
 
     def test_var_db(self):
-        db = self.get_score_db()
+        cdb, db = self.get_score_db()
+        ctx = db._context
         cases = [
-            ['<name>', b'<value>'],
-            ['<name2>', b'<value2>'],
-            ['<name3>', b'<value3>']
+            ['name1', ['s1'], b'<v1>', [VAR_DB_ID, 'name1', 's1']],
+            ['name2', [], b'<v2>', [VAR_DB_ID, 'name2']],
         ]
         for c in cases:
-            _name = c[0]
-            _var = VarDB(_name, db, value_type=bytes)
-            _var.set(c[1])
+            keys = c[1]
+            ddb = db
+            for i in range(len(keys)):
+                ddb = ddb.get_sub_db(keys[i])
 
-            _prefix = VAR_DB_ID + get_encoded_key(_name)
-            value = db.get(_prefix)
-            self.assertEqual(c[1], value)
+            dbase = VarDB(c[0], ddb, value_type=bytes)
+            dbase.set(c[2])
 
-    def test_sub_db(self):
-        db = self.get_score_db()
-        for c in self._dict_cases:
-            _subs = c[1]
-            sub_db = db
-            for i in range(len(_subs)):
-                sub_db = sub_db.get_sub_db(_subs[i])
+            key = b''.join(map(lambda x: get_encoded_key(x), c[3]))
+            self.assertEqual(ContainerUtil.encode_value(c[2]), cdb.get(ctx, sha3_256(key)))
 
-            # VarDB
-            _var = VarDB(c[0], sub_db, value_type=bytes)
-            _var.set(c[2])
+    def test_dict_db_vs_sub_db(self):
+        cdb, db = self.get_score_db()
+        cases = [
+            ['<name1>', ['<sub1>', '<sub2>', '<sub3>'], b'<value1>'],
+            ['<name2>', [1, '<sub2>', '<sub3>'], b'<value2>'],
+            ['<name3>', [1, '<sub2>', '<sub3>', 128], b'<value3>'],
+        ]
+        for c in cases:
+            keys = c[1]
+            depth = len(keys)
 
-            _prefix = VAR_DB_ID + get_encoded_key(c[0])
-            for i in range(len(_subs)):
-                _prefix += get_encoded_key(_subs[i])
-            value = db.get(_prefix)
-            self.assertEqual(c[2], value)
+            # set data only with dictionary
+            sd = DictDB(c[0], db, value_type=bytes, depth=depth)
+            for i in range(depth-1):
+                sd = sd[keys[i]]
+            sd[keys[depth-1]] = c[2]
 
-            # ArrayDB
-            _array = ArrayDB(c[0], sub_db, value_type=bytes)
-            _array.put(c[2])
+            # check stored value in sub_db
+            for sub_depth in range(depth):
+                ddb = db
+                for j in range(sub_depth):
+                    ddb = ddb.get_sub_db(keys[j])
 
-            _prefix = ARRAY_DB_ID + get_encoded_key(c[0])
-            for i in range(len(_subs)):
-                _prefix += get_encoded_key(_subs[i])
-            value = db.get(_prefix + get_encoded_key(b'\x00'))
-            self.assertEqual(c[2], value)
-
-            # DictDB
-            _dict = DictDB(c[0], sub_db, value_type=bytes)
-            _dict[b'<key>'] = c[2]
-
-            _prefix = DICT_DB_ID + get_encoded_key(c[0])
-            for i in range(len(_subs)):
-                _prefix += get_encoded_key(_subs[i])
-            _prefix += get_encoded_key(b'<key>')
-            value = db.get(_prefix)
-            self.assertEqual(c[2], value)
+                d = DictDB(c[0], ddb, value_type=bytes, depth=depth-sub_depth)
+                for k in range(sub_depth, depth-1):
+                    d = d[keys[k]]
+                self.assertEqual(d[keys[depth-1]], c[2])
 
 
 if __name__ == '__main__':
