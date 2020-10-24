542ea68f00b3ab3d4515aae624a26ddc49c5d961
Jaechang Namgoong <jnamgoong@iconloop.com>
2020-10-24 14:14:07 +0900

    Support Java SCORE inheritance relationship

--- javaee/exec/test/java/foundation/icon/ee/SampleTest.java
+++ javaee/exec/test/java/foundation/icon/ee/SampleTest.java
@@ -1,7 +1,10 @@
 package foundation.icon.ee;
 
-import example.SampleToken;
 import collection.CollectionTest;
+import example.InheritedToken;
+import example.SampleToken;
+import example.token.IRC2;
+import example.token.IRC2Basic;
 import foundation.icon.ee.test.GoldenTest;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +16,24 @@ public class SampleTest extends GoldenTest {
     public void testSample() {
         var owner = sm.getOrigin();
         var app = sm.deploy(SampleToken.class, "MySampleToken", "MST", 18, 1000);
+        app.invoke("balanceOf", owner);
+        var addr1 = sm.newExternalAddress();
+        app.invoke("transfer", addr1, new BigInteger("1000000000000000000"), "Hello".getBytes(StandardCharsets.UTF_8));
+        var addr2 = sm.newExternalAddress();
+        app.invoke("transfer", addr2, new BigInteger("1000000000000000000"), "Hello".getBytes(StandardCharsets.UTF_8));
+        app.invoke("balanceOf", addr1);
+        app.invoke("balanceOf", owner);
+        app.invoke("totalSupply");
+        var app2 = sm.deploy(CollectionTest.class);
+        app2.invoke("getInt");
+        app2.invoke("totalSupply2", app.getAddress());
+        app2.invoke("balanceOf2", app.getAddress(), owner);
+    }
+
+    @Test
+    public void testInherited() {
+        var owner = sm.getOrigin();
+        var app = sm.deploy(new Class<?>[]{InheritedToken.class, IRC2Basic.class, IRC2.class}, "MySampleToken", "MST", 18, 1000);
         app.invoke("balanceOf", owner);
         var addr1 = sm.newExternalAddress();
         app.invoke("transfer", addr1, new BigInteger("1000000000000000000"), "Hello".getBytes(StandardCharsets.UTF_8));
---
+++ javaee/samples/sampletoken/src/example/token/IRC2Basic.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2020 ICON Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package example.token;
+
+import score.Address;
+import score.Context;
+import score.DictDB;
+import score.annotation.EventLog;
+import score.annotation.External;
+import score.annotation.Optional;
+
+import java.math.BigInteger;
+
+public abstract class IRC2Basic implements IRC2 {
+    private final String name;
+    private final String symbol;
+    private final int decimals;
+    private final BigInteger totalSupply;
+    private final DictDB<Address, BigInteger> balances;
+
+    public IRC2Basic(String _name, String _symbol, BigInteger _decimals, BigInteger _initialSupply) {
+        this.name = _name;
+        this.symbol = _symbol;
+        this.decimals = _decimals.intValue();
+
+        // decimals must be larger than 0 and less than 21
+        Context.require(this.decimals >= 0);
+        Context.require(this.decimals <= 21);
+
+        // initialSupply must be larger than 0
+        Context.require(_initialSupply.compareTo(BigInteger.ZERO) >= 0);
+
+        // calculate totalSupply
+        if (_initialSupply.compareTo(BigInteger.ZERO) > 0) {
+            BigInteger oneToken = pow(BigInteger.TEN, this.decimals);
+            this.totalSupply = oneToken.multiply(_initialSupply);
+        } else {
+            this.totalSupply = BigInteger.ZERO;
+        }
+
+        // set the initial balance of the owner
+        this.balances = Context.newDictDB("balances", BigInteger.class);
+        this.balances.set(Context.getOrigin(), this.totalSupply);
+    }
+
+    // BigInteger#pow() is not implemented in the shadow BigInteger.
+    // we need to use our implementation for that.
+    private static BigInteger pow(BigInteger base, int exponent) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 0; i < exponent; i++) {
+            result = result.multiply(base);
+        }
+        return result;
+    }
+
+    @External(readonly=true)
+    public String name() {
+        return name;
+    }
+
+    @External(readonly=true)
+    public String symbol() {
+        return symbol;
+    }
+
+    @External(readonly=true)
+    public int decimals() {
+        return decimals;
+    }
+
+    @External(readonly=true)
+    public BigInteger totalSupply() {
+        return totalSupply;
+    }
+
+    @External(readonly=true)
+    public BigInteger balanceOf(Address _owner) {
+        return safeGetBalance(_owner);
+    }
+
+    @External
+    public void transfer(Address _to, BigInteger _value, @Optional byte[] _data) {
+        Address _from = Context.getCaller();
+        BigInteger fromBalance = safeGetBalance(_from);
+        BigInteger toBalance = safeGetBalance(_to);
+
+        // check some basic requirements
+        Context.require(_value.compareTo(BigInteger.ZERO) >= 0);
+        Context.require(fromBalance.compareTo(_value) >= 0);
+
+        // adjust the balances
+        safeSetBalance(_from, fromBalance.subtract(_value));
+        safeSetBalance(_to, toBalance.add(_value));
+
+        // if the recipient is SCORE, call 'tokenFallback' to handle further operation
+        byte[] dataBytes = (_data == null) ? new byte[0] : _data;
+        if (_to.isContract()) {
+            Context.call(_to, "tokenFallback", _from, _value, dataBytes);
+        }
+
+        // emit Transfer event
+        Transfer(_from, _to, _value, dataBytes);
+    }
+
+    protected void _burn(Address owner, BigInteger amount) {
+        // TODO: burn
+    }
+
+    private BigInteger safeGetBalance(Address owner) {
+        return balances.getOrDefault(owner, BigInteger.ZERO);
+    }
+
+    private void safeSetBalance(Address owner, BigInteger amount) {
+        balances.set(owner, amount);
+    }
+
+    @EventLog(indexed=3)
+    public void Transfer(Address _from, Address _to, BigInteger _value, byte[] _data) {}
+}
