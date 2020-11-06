95b5b4d7a38cf13b65e25b741d8d865dd2789739
Hyunil Jung <hijung@iconloop.com>
2020-11-06 18:23:49 +0900

    Add BasicOptimizerTest

---
+++ javaee/exec/test/java/foundation/icon/ee/tooling/BasicOptimizerTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2020 ICON Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package foundation.icon.ee.tooling;
+
+import org.junit.jupiter.api.Test;
+import score.annotation.External;
+
+public class BasicOptimizerTest extends OptimizerGoldenTest {
+    public static class Simple {
+        @External
+        public void external1() {
+        }
+    }
+
+    @Test
+    void simple() {
+        test(Simple.class);
+    }
+
+    public static class Access {
+        public int publicField;
+        protected int protectedField;
+        int packagePrivateField;
+        private int privateField;
+
+        @External
+        public void externalMethod() {
+            publicMethod();
+            protectedMethod();
+            packagePrivateMethod();
+            privateMethod();
+        }
+
+        public void publicMethod() {
+        }
+
+        protected void protectedMethod() {
+        }
+
+        void packagePrivateMethod() {
+        }
+
+        private void privateMethod() {
+        }
+
+        public void unusedPublicMethod() {
+        }
+
+        protected void unusedProtectedMethod() {
+        }
+
+        void unusedPackagePrivateMethod() {
+        }
+
+        private void unusedPrivateMethod() {
+        }
+    }
+
+    @Test
+    void access() {
+        test(Access.class);
+    }
+}
