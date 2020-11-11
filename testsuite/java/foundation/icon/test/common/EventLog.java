bc0b2b900fea332533f6bdc6f7a55482cfb29f86
Jaechang Namgoong <jnamgoong@iconloop.com>
2020-11-11 11:11:55 +0900

    Cleanup testcase code

--- testsuite/java/foundation/icon/test/common/EventLog.java
+++ testsuite/java/foundation/icon/test/common/EventLog.java
@@ -1,12 +1,24 @@
+/*
+ * Copyright 2020 ICON Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package foundation.icon.test.common;
 
 import foundation.icon.icx.data.TransactionResult;
-import foundation.icon.icx.transport.jsonrpc.RpcItem;
-import foundation.icon.icx.transport.jsonrpc.RpcValue;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
 public class EventLog {
@@ -22,13 +34,11 @@ public class EventLog {
         if (score != null && !score.equals(log.getScoreAddress())) {
             return false;
         }
-        var items = new ArrayList<RpcItem>();
-        items.addAll(log.getIndexed());
+        var items = new ArrayList<>(log.getIndexed());
         var data = log.getData();
         if (data != null) {
             items.addAll(data);
         }
-
         for (int idx = 0 ; idx<params.length ; idx++) {
             if (params[idx] == null) continue;
             var item = items.get(idx);
