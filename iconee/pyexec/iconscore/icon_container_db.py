dbde451be9aeaa8c5ae3be2a724a12f1b7792ccd
Jaechang Namgoong <jnamgoong@iconloop.com>
2021-02-24 00:30:35 +0900

    Support ICON Address scheme

--- iconee/pyexec/iconscore/icon_container_db.py
+++ iconee/pyexec/iconscore/icon_container_db.py
@@ -40,7 +40,7 @@ class ContainerUtil(object):
         elif isinstance(key, str):
             bytes_key = key.encode('utf-8')
         elif isinstance(key, Address):
-            bytes_key = key.to_bytes()
+            bytes_key = key.to_canonical_byte()
         elif isinstance(key, bytes):
             bytes_key = key
         else:
