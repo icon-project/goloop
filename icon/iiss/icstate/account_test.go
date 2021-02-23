f40cdc5b7d220022f85fc7632749a155fe17042f
inwonkim <inwonkim@iconloop.com>
2021-02-23 17:20:17 +0900

    fix GetUnbondingInfo bug
    * new unbond did not overwrite unbond when bond and unbond both exist before this commit
    * delete unbond on setBond when bond does not exists, unbond exists
      - refer added test case

--- icon/iiss/icstate/account_test.go
+++ icon/iiss/icstate/account_test.go
@@ -345,6 +345,7 @@ func TestAccount_RemoveUnstaking(t *testing.T) {
 }
 
 func TestAccount_GetUnbondingInfo(t *testing.T) {
+	// case1 2 unbonds will added(for hx3, hx4)
 	a := assTest.Clone()
 	//bonds : [{hx3, 10}, {hx4, 10}] , unbonds : [{address: hx5, value:10, bh: 20}, {hx6, 10, 30}]
 	addr1 := common.NewAddressFromString("hx3")
@@ -364,6 +365,7 @@ func TestAccount_GetUnbondingInfo(t *testing.T) {
 	assert.Equal(t, 0, len(ubMods))
 	assert.Equal(t, 0, uDiff.Cmp(expectedUDiff))
 
+	// case2 hx5 unbond will be modified and hx4 unbond will be added
 	//add bond
 	addr3 := common.NewAddressFromString("hx5")
 	b1 = &Bond{addr3, common.NewHexInt(5)}
@@ -395,6 +397,18 @@ func TestAccount_GetUnbondingInfo(t *testing.T) {
 	assert.True(t, ubAdds[0].Equal(ubAdd1))
 	assert.True(t, ubMods[0].Equal(ubMod1))
 	assert.Equal(t, 1, len(ubAdds))
+	assert.Equal(t, 1, len(ubMods))
+	assert.Equal(t, 0, uDiff.Cmp(expectedUDiff))
+
+	// hx6 unbond will be modified(removed)
+	addr4 := common.NewAddressFromString("hx6")
+	b1 = &Bond{addr4, common.NewHexInt(3)}
+	nbs = []*Bond{b1}
+	ubAdds, ubMods, uDiff = a.GetUnbondingInfo(nbs, bh)
+	expectedUDiff = big.NewInt(-10)
+	ubMod1 = &Unbond{addr4, big.NewInt(0), bh}
+	assert.Equal(t, 0, len(ubAdds))
+	assert.True(t, ubMods[0].Equal(ubMod1))
 	assert.Equal(t, 1, len(ubMods))
 	assert.Equal(t, 0, uDiff.Cmp(expectedUDiff))
 }
