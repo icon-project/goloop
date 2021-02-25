icon-project/goloop

commit 855d2966e27b1357d73576e18afa33499df7f296

Optimize comparison logics of MPT and DepositList

common/trie/ompt/mpt.go
@@ -59,6 +59,12 @@ func bytesToNibs(k []byte) []byte {
 	return nibs
 }
 
+func (mb *mptBase) Equal(mb2 *mptBase) bool {
+	return mb.db == mb2.db &&
+		mb.bucket == mb2.bucket &&
+		mb.objectType == mb2.objectType
+}
+
 func (m *mpt) bytesToNibs(k []byte) []byte {
 	ks := len(k)
 	if cap(m.nibs) < ks*2 {
@@ -270,7 +276,7 @@ func (m *mpt) Reset(s trie.ImmutableForObject) {
 	}
 
 	m2, ok := s.(*mpt)
-	if (!ok) || !reflect.DeepEqual(m2.mptBase, m.mptBase) {
+	if (!ok) || !m2.mptBase.Equal(&m.mptBase) {
 		log.Panicln("Supplied ImmutableForObject isn't usable in here", s)
 	}
 	m.root = m2.root

service/state/deposit.go
@@ -49,6 +49,7 @@ type depositImpl interface {
 	ConsumeDepositLv1(height int64, amount *big.Int) *big.Int
 	ConsumeDepositLv2(height int64, amount *big.Int) *big.Int
 	Clone() depositImpl
+	Equal(depositImpl) bool
 }
 
 type deposit struct {
@@ -89,6 +90,10 @@ func (dp *deposit) RLPDecodeSelf(d codec.Decoder) error {
 			"InvalidDepositVersion(version=%d)", version)
 	}
 	return dp.depositImpl.RLPDecodeFields(d2)
+}
+
+func (dp *deposit) Equal(dp2 *deposit) bool {
+	return dp.depositImpl.Equal(dp2.depositImpl)
 }
 
 type depositV1 struct {
@@ -290,6 +295,22 @@ func (d *depositV1) Clone() depositImpl {
 	return d2
 }
 
+func (d *depositV1) Equal(d2 depositImpl) bool {
+	if d2p, ok := d2.(*depositV1); !ok {
+		return false
+	} else {
+		if d == d2p {
+			return true
+		}
+		return bytes.Equal(d.ID, d2p.ID) &&
+			d.DepositAmount.Cmp(d2p.DepositAmount) == 0 &&
+			d.DepositRemain.Cmp(d2p.DepositRemain) == 0 &&
+			d.ExpireHeight == d2p.ExpireHeight &&
+			d.StepIssued.Cmp(d2p.StepIssued) == 0 &&
+			d.StepRemain.Cmp(d2p.StepRemain) == 0
+	}
+}
+
 func newDepositV1(dc DepositContext, value *big.Int) (*depositV1, error) {
 	issue := calcVirtualSteps(value, dc.DepositIssueRate(), dc.StepPrice())
 	return &depositV1{
@@ -390,6 +411,14 @@ func (d *depositV2) Clone() depositImpl {
 	d2 := new(depositV2)
 	*d2 = *d
 	return d2
+}
+
+func (d *depositV2) Equal(d2 depositImpl) bool {
+	if d2p, ok := d2.(*depositV2); !ok {
+		return false
+	} else {
+		return d.DepositRemain.Cmp(d2p.DepositRemain) == 0
+	}
 }
 
 func newDepositV2(dc DepositContext, value *big.Int) (*depositV2, error) {

service/state/depositlist.go
@@ -18,7 +18,6 @@ package state
 
 import (
 	"math/big"
-	"reflect"
 
 	"github.com/icon-project/goloop/common/intconv"
 	"github.com/icon-project/goloop/module"
@@ -47,7 +46,15 @@ func (dl depositList) Has() bool {
 }
 
 func (dl depositList) Equal(di2 depositList) bool {
-	return reflect.DeepEqual([]*deposit(dl), []*deposit(di2))
+	if len(dl) != len(di2) {
+		return false
+	}
+	for idx, dp := range dl {
+		if !dp.Equal(di2[idx]) {
+			return false
+		}
+	}
+	return true
 }
 
 func (dl depositList) Clone() depositList {

commit c17add1347f15042d47336c7093381bea688095f

Fix problem, comparison failure in mptBase.Equal()
While it compares MPT trie, it may fail even though it should succeed.
It's caused by commit 855d2966e27b1357d73576e18afa33499df7f296.

common/trie/ompt/mpt.go
@@ -61,7 +61,6 @@ func bytesToNibs(k []byte) []byte {
 
 func (mb *mptBase) Equal(mb2 *mptBase) bool {
 	return mb.db == mb2.db &&
-		mb.bucket == mb2.bucket &&
 		mb.objectType == mb2.objectType
 }
 