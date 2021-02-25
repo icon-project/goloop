4546863fbeabcf3c9b40ea51c8fb8677a8dbbe26
MoonKyu Song <mksong@iconloop.com>
2021-02-25 16:33:06 +0900

    Improve intconv.ParseBigInt support decimals

--- common/intconv/string_test.go
+++ common/intconv/string_test.go
@@ -1,6 +1,9 @@
 package intconv
 
-import "testing"
+import (
+	"math/big"
+	"testing"
+)
 
 func TestParseUint(t *testing.T) {
 	type args struct {
@@ -49,6 +52,7 @@ func TestParseInt(t *testing.T) {
 		{"T5", args{"0x0ffff", 16}, 0, true},
 		{"T6", args{"-0x8000000000000000", 64}, -0x8000000000000000, false},
 		{"T7", args{"-0x10000000000000000", 64}, 0, true},
+		{"T8", args{"-10", 64}, -10, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -59,6 +63,54 @@ func TestParseInt(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("ParseInt() = %v, want %v", got, tt.want)
+			}
+			var got2 big.Int
+			err = ParseBigInt(&got2, tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseBigInt() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			exp := big.NewInt(tt.want)
+			if exp.Cmp(&got2) != 0 {
+				t.Errorf("ParseBigInt() = %v, want %v", &got2, tt.want)
+			}
+		})
+	}
+}
+
+func TestParseBigInt(t *testing.T) {
+	type args struct {
+		s string
+	}
+	n1 := new(big.Int).SetBytes([]byte{0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+	n1.Neg(n1)
+
+	tests := []struct {
+		name    string
+		args    args
+		want    *big.Int
+		wantErr bool
+	}{
+		{"T1", args{"0x0"}, big.NewInt(0), false},
+		{"T2", args{"0x7fff"}, big.NewInt(0x7fff), false},
+		{"T3", args{"-0x8000000000000000"}, big.NewInt(-0x8000000000000000), false},
+		{"T4", args{"-0x10000000000000000"}, n1, false},
+		{"T5", args{"-18446744073709551616"}, n1, false},
+		{"T6", args{"-1844674407370955161a"}, nil, true},
+		{"T7", args{"887234"}, big.NewInt(887234), false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var got big.Int
+			err := ParseBigInt(&got, tt.args.s)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ParseBigInt() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if err == nil {
+				if tt.want.Cmp(&got) != 0 {
+					t.Errorf("ParseBigInt() = %v, want %v", &got, tt.want)
+				}
 			}
 		})
 	}
