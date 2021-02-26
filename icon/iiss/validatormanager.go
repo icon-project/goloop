ccfaccf0291049224de847a838e9253606a8d048
Chiwon Cho <superdoli@gmail.com>
2021-02-26 17:25:31 +0900

    Add ValidatorManager

---
+++ icon/iiss/validatormanager.go
@@ -0,0 +1,265 @@
+package iiss
+
+import (
+	"container/list"
+	"github.com/icon-project/goloop/common"
+	"github.com/icon-project/goloop/common/errors"
+	"github.com/icon-project/goloop/common/log"
+	"github.com/icon-project/goloop/icon/iiss/icstate"
+	"github.com/icon-project/goloop/icon/iiss/icutils"
+	"github.com/icon-project/goloop/module"
+)
+
+type ValidatorImpl struct {
+	added   bool
+	address module.Address
+}
+
+func (vi *ValidatorImpl) IsAdded() bool {
+	return vi.added
+}
+
+func (vi *ValidatorImpl) ResetFlags() {
+	vi.added = false
+}
+
+func (vi *ValidatorImpl) Address() module.Address {
+	return vi.address
+}
+
+func (vi *ValidatorImpl) PublicKey() []byte {
+	return nil
+}
+
+func (vi *ValidatorImpl) Bytes() []byte {
+	return vi.address.Bytes()
+}
+
+func validatorFromAddress(a module.Address, added bool) (module.Validator, error) {
+	if a == nil {
+		return nil, errors.ErrIllegalArgument
+	}
+	if a.IsContract() {
+		return nil, errors.ErrIllegalArgument
+	}
+	v := &ValidatorImpl{
+		added:   added,
+		address: common.AddressToPtr(a),
+	}
+	return v, nil
+}
+
+type ValidatorManager struct {
+	icutils.Immutable
+
+	updated bool
+	pssIdx  int // The index of next validator candidate in term
+	vlist   *list.List
+	vmap    map[string]*list.Element
+}
+
+func (vm *ValidatorManager) Hash() []byte {
+	return nil
+}
+
+func (vm *ValidatorManager) Bytes() []byte {
+	return nil
+}
+
+func (vm *ValidatorManager) Flush() error {
+	return nil
+}
+
+func (vm *ValidatorManager) IndexOf(address module.Address) int {
+	i := 0
+	for e := vm.vlist.Front(); e != nil; e = e.Next() {
+		v := e.Value.(module.Validator)
+		if address.Equal(v.Address()) {
+			return i
+		}
+		i++
+	}
+	return -1
+}
+
+func (vm *ValidatorManager) Len() int {
+	return vm.vlist.Len()
+}
+
+func (vm *ValidatorManager) PRepSnapshotIndex() int {
+	return vm.pssIdx
+}
+
+func (vm *ValidatorManager) SetPRepSnapshotIndex(idx int) {
+	vm.pssIdx = idx
+}
+
+func (vm *ValidatorManager) Get(idx int) (module.Validator, bool) {
+	size := vm.Len()
+	if idx < 0 || idx > size {
+		return nil, false
+	}
+
+	e := vm.vlist.Front()
+	for i := 0; i < idx; i++ {
+		e = e.Next()
+	}
+	return e.Value.(module.Validator), true
+}
+
+func (vm *ValidatorManager) IsUpdated() bool {
+	return vm.updated
+}
+
+func (vm *ValidatorManager) SetUpdated(on bool) {
+	vm.updated = on
+}
+
+func (vm *ValidatorManager) ResetUpdated() {
+	vm.updated = false
+}
+
+func (vm *ValidatorManager) Add(node module.Address) error {
+	return vm.add(node, true)
+}
+
+func (vm *ValidatorManager) add(node module.Address, added bool) error {
+	if err := vm.checkWritable(); err != nil {
+		return err
+	}
+	if node == nil {
+		return errors.Errorf("Node address is nil")
+	}
+
+	key := icutils.ToKey(node)
+	if _, ok := vm.vmap[key]; ok {
+		return nil
+	}
+
+	v, err := validatorFromAddress(node, added)
+	if err != nil {
+		return err
+	}
+
+	e := vm.vlist.PushBack(v)
+	vm.vmap[key] = e
+	vm.updated = true
+	return nil
+}
+
+func (vm *ValidatorManager) Replace(oldNode, newNode module.Address) error {
+	key := icutils.ToKey(oldNode)
+	e, ok := vm.vmap[key]
+	if !ok {
+		return errors.Errorf("Node not found: %s", oldNode)
+	}
+	_, ok = vm.vmap[icutils.ToKey(newNode)]
+	if ok {
+		return errors.Errorf("Node already in use: %s", newNode)
+	}
+
+	v, err := validatorFromAddress(newNode, false)
+	if err != nil {
+		return err
+	}
+
+	e.Value = v
+	vm.updated = true
+	return nil
+}
+
+func (vm *ValidatorManager) Remove(node module.Address) error {
+	if err := vm.checkWritable(); err != nil {
+		return err
+	}
+
+	key := icutils.ToKey(node)
+	e, ok := vm.vmap[key]
+	if !ok {
+		return errors.Errorf("Node not found: %s", node)
+	}
+
+	vm.vlist.Remove(e)
+	delete(vm.vmap, key)
+	vm.updated = true
+	return nil
+}
+
+func (vm *ValidatorManager) Clear() error {
+	vm.updated = false
+	vm.vlist = vm.vlist.Init()
+	vm.vmap = make(map[string]*list.Element)
+	vm.updated = false
+	return nil
+}
+
+func (vm *ValidatorManager) GetValidators() ([]module.Validator, error) {
+	size := vm.Len()
+	vs := make([]module.Validator, size, size)
+	e := vm.vlist.Front()
+	for i := 0; i < size; i++ {
+		vs[i] = e.Value.(module.Validator)
+		e = e.Next()
+	}
+	return vs, nil
+}
+
+func (vm *ValidatorManager) Init(pm *PRepManager, term *icstate.Term) error {
+	if vm.Len() > 0 {
+		return errors.Errorf("ValidatorManager is not empty")
+	}
+	return vm.load(pm, term, false)
+}
+
+func (vm *ValidatorManager) Load(pm *PRepManager, term *icstate.Term) error {
+	return vm.load(pm, term, true)
+}
+
+func (vm *ValidatorManager) load(pm *PRepManager, term *icstate.Term, added bool) error {
+	if err := vm.checkWritable(); err != nil {
+		return err
+	}
+
+	mainPReps := pm.GetPRepSize(icstate.Main)
+	if mainPReps <= vm.Len() {
+		return errors.Errorf(
+			"Invalid mainPRepCount: mainPRepCount(%d) <= validatorCount(%d)",
+			mainPReps,
+			vm.Len(),
+		)
+	}
+
+	pssCount := term.GetPRepSnapshotCount()
+	for i := vm.pssIdx; i < pssCount; i++ {
+		pss := term.GetPRepSnapshotByIndex(i)
+		prep := pm.GetPRepByOwner(pss.Owner())
+		if prep == nil {
+			log.Infof("PRep not found in ValidatorManager: %s", pss.Owner())
+			continue
+		}
+		if prep.Grade() == icstate.Main {
+			if err := vm.add(prep.GetNode(), added); err != nil {
+				return err
+			}
+			if vm.Len() == mainPReps {
+				vm.pssIdx = i + 1
+				break
+			}
+		}
+	}
+	return nil
+}
+
+func (vm *ValidatorManager) checkWritable() error {
+	if vm.IsReadonly() {
+		return errors.Errorf("Writing is not allowed: %v", vm)
+	}
+	return nil
+}
+
+func NewValidatorManager() *ValidatorManager {
+	return &ValidatorManager{
+		vlist: list.New(),
+		vmap:  make(map[string]*list.Element),
+	}
+}
---
+++ icon/iiss/validatormanager_test.go
@@ -0,0 +1,132 @@
+package iiss
+
+import (
+	"fmt"
+	"github.com/icon-project/goloop/common"
+	"github.com/icon-project/goloop/module"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func newAddress(idx int) module.Address {
+	value := fmt.Sprintf("hx%02x", idx)
+	return common.MustNewAddressFromString(value)
+}
+
+func newDummyValidatorManager(t *testing.T, size int) *ValidatorManager {
+	vm := NewValidatorManager()
+
+	for i := 0; i < size; i++ {
+		address := newAddress(i)
+		err := vm.add(address, true)
+		assert.NoError(t, err)
+
+		v, _ := vm.Get(i)
+		vi := v.(*ValidatorImpl)
+		assert.True(t, vi.IsAdded())
+	}
+	assert.Equal(t, size, vm.Len())
+
+	return vm
+}
+
+func TestNewValidatorManager(t *testing.T) {
+	vm := NewValidatorManager()
+	assert.Zero(t, vm.Len())
+}
+
+func TestValidatorManager_add(t *testing.T) {
+	size := 10
+	vm := newDummyValidatorManager(t, size)
+	assert.Equal(t, size, vm.Len())
+}
+
+func TestValidatorManager_GetValidators(t *testing.T) {
+	size := 22
+	vm := newDummyValidatorManager(t, size)
+
+	vs, err := vm.GetValidators()
+	assert.NoError(t, err)
+	assert.Equal(t, size, len(vs))
+
+	for i := 0; i < size; i++ {
+		expected, _ := vm.Get(i)
+		assert.Equal(t, expected, vs[i])
+	}
+}
+
+func TestValidatorManager_Remove(t *testing.T) {
+	var err error
+	size := 22
+	vm := newDummyValidatorManager(t, size)
+
+	idx := size / 2
+	address := newAddress(idx)
+
+	v, ok := vm.Get(idx)
+	assert.True(t, ok)
+	assert.True(t, address.Equal(v.Address()))
+
+	i := vm.IndexOf(address)
+	assert.Equal(t, idx, i)
+
+	err = vm.Remove(address)
+	assert.NoError(t, err)
+	assert.Equal(t, size-1, vm.Len())
+
+	idx = vm.IndexOf(address)
+	assert.True(t, idx < 0)
+
+	err = vm.Remove(address)
+	assert.Error(t, err)
+
+	idx = vm.IndexOf(address)
+	assert.True(t, idx < 0)
+}
+
+func TestValidatorManager_Replace(t *testing.T) {
+	size := 22
+	vm := newDummyValidatorManager(t, size)
+	assert.Equal(t, size, vm.Len())
+
+	for i := 0; i < size; i++ {
+		v, ok := vm.Get(i)
+		assert.NotNil(t, v)
+		assert.True(t, ok)
+
+		newAddr := newAddress(i)
+		err := vm.Replace(v.Address(), newAddr)
+		assert.Error(t, err)
+	}
+
+	for i := 0; i < size; i++ {
+		v, ok := vm.Get(i)
+		assert.NotNil(t, v)
+		assert.True(t, ok)
+
+		newAddr := newAddress(i + 100)
+		err := vm.Replace(v.Address(), newAddr)
+		assert.NoError(t, err)
+
+		idx := vm.IndexOf(newAddr)
+		assert.Equal(t, i, idx)
+
+		idx = vm.IndexOf(v.Address())
+		assert.True(t, idx < 0)
+
+		v, ok = vm.Get(i)
+		assert.True(t, ok)
+		assert.True(t, newAddr.Equal(v.Address()))
+	}
+}
+
+func TestValidatorManager_Clear(t *testing.T) {
+	size := 22
+	vm := newDummyValidatorManager(t, size)
+	assert.Equal(t, size, vm.Len())
+
+	err := vm.Clear()
+	assert.False(t, vm.IsUpdated())
+	assert.NoError(t, err)
+	assert.Zero(t, vm.Len())
+}
