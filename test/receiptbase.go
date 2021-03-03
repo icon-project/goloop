cadf8d234dbd72016e464b3c7708f72e5198459b
MoonKyu Song <mksong@iconloop.com>
2021-03-03 18:46:01 +0900

    Add missing implementation of test.ReceiptBase

--- test/receiptbase.go
+++ test/receiptbase.go
@@ -53,6 +53,10 @@ func (_r *ReceiptBase) EventLogIterator() module.EventLogIterator {
 	panic("not implemented")
 }
 
+func (_r *ReceiptBase) FeePaymentIterator() module.FeePaymentIterator {
+	panic("not implemented")
+}
+
 func (_r *ReceiptBase) GetProofOfEvent(int) ([][]byte, error) {
 	panic("not implemented")
 }
