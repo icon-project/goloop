a8ba4f5d0157520befeb441f41dc9be2c280f02f
Jaechang Namgoong <jnamgoong@iconloop.com>
2021-03-04 23:13:45 +0900

    Add setRevision handling

--- iconee/pyexec/icon_constant.py
+++ iconee/pyexec/icon_constant.py
@@ -22,7 +22,8 @@ DATA_BYTE_ORDER = 'big'  # big endian
 
 # Reserved EventLog
 ICX_TRANSFER_EVENT_LOG = 'ICXTransfer(Address,Address,int)'
-GOV_REJECTED_EVENT_LOG = 'Rejected(str,str)'
+GOV_EVENT_REJECTED = 'Rejected(str,str)'
+GOV_EVENT_REVISION_CHANGED = 'RevisionChanged(int,str)'
 
 
 # Revisions
