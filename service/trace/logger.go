ad83260d654ab7d027326102e7bdf5f6d0032326
MoonKyu Song <mksong@iconloop.com>
2021-03-22 13:59:02 +0900

    Reduce trace log overhead

--- service/trace/logger.go
+++ service/trace/logger.go
@@ -13,56 +13,62 @@ type Logger struct {
 	onLog   func(lv module.TraceLevel, msg string)
 }
 
-func (l *Logger) TLog(lv module.TraceLevel, a ...interface{}) {
-	l.onLog(lv, fmt.Sprint(a...))
-}
-
 func (l *Logger) IsTrace() bool {
 	return l.isTrace
 }
 
-func (l *Logger) TLogln(lv module.TraceLevel, f string, a ...interface{}) {
-	l.onLog(lv, fmt.Sprint(a...))
+func (l *Logger) TLog(lv module.TraceLevel, a ...interface{}) {
+	if l.isTrace {
+		l.onLog(lv, fmt.Sprint(a...))
+	}
+}
+
+func (l *Logger) TLogln(lv module.TraceLevel, a ...interface{}) {
+	if l.isTrace {
+		l.onLog(lv, fmt.Sprint(a...))
+	}
 }
 
 func (l *Logger) TLogf(lv module.TraceLevel, f string, a ...interface{}) {
-	l.onLog(lv, fmt.Sprintf(f, a...))
+	if l.isTrace {
+		l.onLog(lv, fmt.Sprintf(f, a...))
+	}
 }
 
 func (l *Logger) TDebug(a ...interface{}) {
-	l.onLog(module.TDebugLevel, fmt.Sprint(a...))
+	l.TLog(module.TDebugLevel, a...)
 }
 
 func (l *Logger) TDebugln(a ...interface{}) {
-	l.onLog(module.TDebugLevel, fmt.Sprint(a...))
+	l.TLogln(module.TDebugLevel, a...)
 }
 
 func (l *Logger) TDebugf(f string, a ...interface{}) {
-	l.onLog(module.TDebugLevel, fmt.Sprintf(f, a...))
+	l.TLogf(module.TDebugLevel, f, a...)
 }
 
 func (l *Logger) TTrace(a ...interface{}) {
-	l.onLog(module.TTraceLevel, fmt.Sprint(a...))
+	l.TLog(module.TTraceLevel, a...)
 }
 
 func (l *Logger) TTraceln(a ...interface{}) {
-	l.onLog(module.TTraceLevel, fmt.Sprint(a...))
+	l.TLogln(module.TTraceLevel, a...)
 }
 
 func (l *Logger) TTracef(f string, a ...interface{}) {
-	l.onLog(module.TTraceLevel, fmt.Sprintf(f, a...))
+	l.TLogf(module.TTraceLevel, f, a...)
 }
 
 func (l *Logger) TSystem(a ...interface{}) {
-	l.onLog(module.TSystemLevel, fmt.Sprint(a...))
+	l.TLog(module.TSystemLevel, a...)
 }
 
 func (l *Logger) TSystemln(a ...interface{}) {
-	l.onLog(module.TSystemLevel, fmt.Sprint(a...))
+	l.TLogln(module.TSystemLevel, a...)
 }
 
 func (l *Logger) TSystemf(f string, a ...interface{}) {
-	l.onLog(module.TSystemLevel, fmt.Sprintf(f, a...))
+	l.TLogf(module.TSystemLevel, f, a...)
 }
 
 func (l *Logger) WithFields(f log.Fields) log.Logger {
