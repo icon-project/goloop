b9b032cf0566d9a832ba409f089cc9eb4c142acb
Hyunil Jung <hijung@iconloop.com>
2021-05-03 12:25:18 +0900

    Removed unnecessary code

--- icon/lcimporter/json.go
+++ icon/lcimporter/json.go
@@ -17,36 +17,14 @@
 package lcimporter
 
 import (
-	"bytes"
 	"encoding/json"
 
-	"github.com/icon-project/goloop/common/errors"
 	"github.com/icon-project/goloop/common/log"
 	"github.com/icon-project/goloop/module"
 )
 
 type ToJSONer interface {
 	ToJSON(version module.JSONVersion) (interface{}, error)
-}
-
-func JSONMarshalAndCompact(jso interface{}) ([]byte, error) {
-	if jsoner, ok := jso.(ToJSONer); ok {
-		if obj, err := jsoner.ToJSON(module.JSONVersionLast); err != nil {
-			return nil, err
-		} else {
-			jso = obj
-		}
-	}
-	bs, err := json.Marshal(jso)
-	if err != nil {
-		log.Warnf("marshalling failure obj=%+v", jso)
-		return nil, err
-	}
-	bf := bytes.NewBuffer(nil)
-	if err := json.Compact(bf, bs); err != nil {
-		return nil, errors.Wrapf(err, "CompactFailure(bs=%q)", string(bs))
-	}
-	return bf.Bytes(), nil
 }
 
 func JSONMarshalIndent(obj interface{}) ([]byte, error) {
