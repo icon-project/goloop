e756cab90f0f0418655ede623c32fba2f26a6f8d
MoonKyu Song <mksong@iconloop.com>
2021-07-05 11:41:27 +0900

    Ignore estimation with base transaction

--- icon/iiss/base.go
+++ icon/iiss/base.go
@@ -124,6 +124,9 @@ func (tx *baseV3) Prepare(ctx contract.Context) (state.WorldContext, error) {
 }
 
 func (tx *baseV3) Execute(ctx contract.Context, estimate bool) (txresult.Receipt, error) {
+	if estimate {
+		return nil, errors.InvalidStateError.New("EstimationNotAllowed")
+	}
 	info := ctx.TransactionInfo()
 	if info == nil {
 		return nil, errors.InvalidStateError.New("TransactionInfoUnavailable")
