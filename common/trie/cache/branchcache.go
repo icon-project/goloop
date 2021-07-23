e1f9d9549a4bf8659c70d493d59f5902ccdcb679
MoonKyu Song <mksong@iconloop.com>
2021-07-23 15:17:35 +0900

    Support fullcache for nodecache

---
+++ common/trie/cache/branchcache.go
@@ -0,0 +1,182 @@
+/*
+ * Copyright 2021 ICON Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cache
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/icon-project/goloop/common/log"
+)
+
+const (
+	hashSize          = 32
+	dataMaxSize       = 532
+	cacheItemSize     = hashSize + dataMaxSize
+	fileCacheItemSize = cacheItemSize + 2
+)
+
+type BranchCache struct {
+	nodes  [][2][]byte
+	offset int
+	depth  int
+	size   int
+	f      *os.File
+}
+
+func (c *BranchCache) Get(nibs []byte, h []byte) ([]byte, bool) {
+	if c == nil || nibs == nil || len(nibs) >= c.depth {
+		return nil, false
+	}
+	idx := indexByNibs(nibs)
+
+	var node [][]byte
+	if idx < c.offset {
+		node = c.nodes[idx][:]
+	} else {
+		node = c.read(idx)
+		if node == nil {
+			return nil, true
+		}
+	}
+	if bytes.Equal(node[0], h) {
+		return node[1], true
+	}
+	return nil, true
+}
+
+func (c *BranchCache) String() string {
+	return fmt.Sprintf("BranchCache{%p depth=%d offset=%d}", c, c.depth, c.offset)
+}
+
+func (c *BranchCache) Put(nibs []byte, h []byte, serialized []byte) {
+	if nibs == nil || len(serialized) > dataMaxSize || len(nibs) >= c.depth {
+		return
+	}
+	idx := indexByNibs(nibs)
+
+	if idx < c.offset {
+		c.nodes[idx] = [2][]byte{h, serialized}
+	} else {
+		c.write(idx, h, serialized)
+	}
+}
+
+func (c *BranchCache) read(idx int) [][]byte {
+	at, err := c.f.Seek(int64((idx-c.offset)*fileCacheItemSize), 0)
+	if err != nil {
+		//return nil, fmt.Errorf("fail to seek err:%+v", err)
+		return nil
+	}
+	var node [2][]byte
+	b := make([]byte, fileCacheItemSize)
+	n, err := c.f.ReadAt(b, at)
+	if err != nil {
+		//return nil, fmt.Errorf("fail to read err:%+v", err)
+		return nil
+	}
+	if n < 2 {
+		//return nil, fmt.Errorf("fail to read prefix")
+		return nil
+	}
+	vl := binary.BigEndian.Uint16(b[:2])
+	if vl < hashSize {
+		return nil
+	}
+	l := 2 + int(vl)
+	if n < l {
+		//return nil, fmt.Errorf("fail to read n:%d, expected:%d",n,l)
+		return nil
+	}
+	b = b[2:]
+	node[0] = b[:hashSize]
+	node[1] = b[hashSize:vl]
+	return node[:]
+}
+
+func (c *BranchCache) write(idx int, h []byte, serialized []byte) {
+	at := int64((idx - c.offset) * fileCacheItemSize)
+	vl := hashSize + len(serialized)
+	b := make([]byte, 2, 2+vl)
+	binary.BigEndian.PutUint16(b[:2], uint16(vl))
+	b = append(b, h...)
+	b = append(b, serialized...)
+	if _, err := c.f.WriteAt(b, at); err != nil {
+		c.size = c.offset
+		c.f.Close()
+	}
+}
+
+func openfile(path string) (f *os.File, err error) {
+	dir := filepath.Dir(path)
+	if fi, sErr := os.Stat(dir); sErr == nil {
+		if !fi.IsDir() {
+			err = fmt.Errorf("open %s: not a directory", dir)
+			return
+		}
+	} else if os.IsNotExist(sErr) {
+		if mkErr := os.MkdirAll(dir, 0755); mkErr != nil {
+			err = fmt.Errorf("fail to mkdir err:%+v", mkErr)
+			return
+		}
+	} else {
+		err = fmt.Errorf("fail to stat err:%+v", sErr)
+		return
+	}
+	f, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		err = fmt.Errorf("fail to open path:%s, err:%+v", path, err)
+		return
+	}
+	return f, nil
+}
+
+func (c *BranchCache) OnAttach(count int32, id []byte) cacheImpl {
+	if count >= fullCacheMigrationThreshold {
+		if logCacheEvents {
+			log.Warnf("MigrateCacheFor(id=%#x, count=%d)", id, count)
+		}
+		return NewFullCacheFromBranch(c)
+	}
+	return c
+}
+
+func NewBranchCache(depth int, fdepth int, path string) *BranchCache {
+	offset := sizeByDepth(depth)
+	size := sizeByDepth(depth + fdepth)
+	var f *os.File
+	if fdepth > 0 {
+		var err error
+		if f, err = openfile(path); err != nil {
+			log.Infof("BranchCache fdepth:%d will be ignored, err:%+v", fdepth, err)
+			size = offset
+		} else {
+			abs, _ := filepath.Abs(f.Name())
+			log.Debugf("BranchCache using fdepth:%d path:%s", fdepth, abs)
+		}
+	}
+	return &BranchCache{
+		nodes:  make([][2][]byte, offset),
+		offset: offset,
+		depth:  depth + fdepth,
+		size:   size,
+		f:      f,
+	}
+}
--- common/trie/cache/nodecache.go
+++ common/trie/cache/nodecache.go
@@ -1,31 +1,14 @@
 package cache
 
 import (
-	"bytes"
-	"encoding/binary"
 	"fmt"
-	"os"
-	"path/filepath"
 	"sync"
-
-	"github.com/icon-project/goloop/common/log"
+	"sync/atomic"
 )
 
 const (
-	hashSize          = 32
-	dataMaxSize       = 532
-	cacheItemSize     = hashSize + dataMaxSize
-	fileCacheItemSize = cacheItemSize + 2
+	fullCacheMigrationThreshold = 150
 )
-
-type NodeCache struct {
-	lock   sync.Mutex
-	nodes  [][2][]byte
-	offset int
-	depth  int
-	size   int
-	f      *os.File
-}
 
 func indexByNibs(nibs []byte) int {
 	idx := 0
@@ -39,138 +22,53 @@ func sizeByDepth(d int) int {
 	return ((1 << uint(4*d)) - 1) / 15
 }
 
+type cacheImpl interface {
+	Get(nibs []byte, h []byte) ([]byte, bool)
+	Put(nibs []byte, h []byte, serialized []byte)
+	OnAttach(cnt int32, id []byte) cacheImpl
+}
+
+type NodeCache struct {
+	lock     sync.Mutex
+	impl     cacheImpl
+	countGet int32
+}
+
 func (c *NodeCache) Get(nibs []byte, h []byte) ([]byte, bool) {
-	if c == nil || nibs == nil || len(nibs) >= c.depth {
+	if c == nil {
 		return nil, false
 	}
-	idx := indexByNibs(nibs)
-
 	c.lock.Lock()
 	defer c.lock.Unlock()
 
-	var node [][]byte
-	if idx < c.offset {
-		node = c.nodes[idx][:]
-	} else {
-		node = c.read(idx)
-		if node == nil {
-			return nil, true
-		}
-	}
-	if bytes.Equal(node[0], h) {
-		return node[1], true
-	}
-	return nil, true
+	atomic.AddInt32(&c.countGet, 1)
+	return c.impl.Get(nibs, h)
 }
 
 func (c *NodeCache) String() string {
-	return fmt.Sprintf("NodeCache{%p depth=%d offset=%d}", c, c.depth, c.offset)
+	return fmt.Sprintf("NodeCache{%v}", c.impl)
 }
 
 func (c *NodeCache) Put(nibs []byte, h []byte, serialized []byte) {
-	if c == nil || nibs == nil || len(serialized) > dataMaxSize || len(nibs) >= c.depth {
+	if c == nil {
 		return
 	}
-	idx := indexByNibs(nibs)
-
 	c.lock.Lock()
 	defer c.lock.Unlock()
-
-	if idx < c.offset {
-		c.nodes[idx] = [2][]byte{h, serialized}
-	} else {
-		c.write(idx, h, serialized)
-	}
+	c.impl.Put(nibs, h, serialized)
 }
 
-func (c *NodeCache) read(idx int) [][]byte {
-	at, err := c.f.Seek(int64((idx-c.offset)*fileCacheItemSize), 0)
-	if err != nil {
-		//return nil, fmt.Errorf("fail to seek err:%+v", err)
-		return nil
+func (c *NodeCache) OnAttach(id []byte) *NodeCache {
+	if c != nil {
+		cnt := atomic.SwapInt32(&c.countGet, 0)
+		c.impl = c.impl.OnAttach(cnt, id)
 	}
-	var node [2][]byte
-	b := make([]byte, fileCacheItemSize)
-	n, err := c.f.ReadAt(b, at)
-	if err != nil {
-		//return nil, fmt.Errorf("fail to read err:%+v", err)
-		return nil
-	}
-	if n < 2 {
-		//return nil, fmt.Errorf("fail to read prefix")
-		return nil
-	}
-	vl := binary.BigEndian.Uint16(b[:2])
-	if vl < hashSize {
-		return nil
-	}
-	l := 2 + int(vl)
-	if n < l {
-		//return nil, fmt.Errorf("fail to read n:%d, expected:%d",n,l)
-		return nil
-	}
-	b = b[2:]
-	node[0] = b[:hashSize]
-	node[1] = b[hashSize:vl]
-	return node[:]
-}
-
-func (c *NodeCache) write(idx int, h []byte, serialized []byte) {
-	at := int64((idx - c.offset) * fileCacheItemSize)
-	vl := hashSize + len(serialized)
-	b := make([]byte, 2, 2+vl)
-	binary.BigEndian.PutUint16(b[:2], uint16(vl))
-	b = append(b, h...)
-	b = append(b, serialized...)
-	if _, err := c.f.WriteAt(b, at); err != nil {
-		c.size = c.offset
-		c.f.Close()
-	}
-}
-
-func openfile(path string) (f *os.File, err error) {
-	dir := filepath.Dir(path)
-	if fi, sErr := os.Stat(dir); sErr == nil {
-		if !fi.IsDir() {
-			err = fmt.Errorf("open %s: not a directory", dir)
-			return
-		}
-	} else if os.IsNotExist(sErr) {
-		if mkErr := os.MkdirAll(dir, 0755); mkErr != nil {
-			err = fmt.Errorf("fail to mkdir err:%+v", mkErr)
-			return
-		}
-	} else {
-		err = fmt.Errorf("fail to stat err:%+v", sErr)
-		return
-	}
-	f, err = os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
-	if err != nil {
-		err = fmt.Errorf("fail to open path:%s, err:%+v", path, err)
-		return
-	}
-	return f, nil
+	return c
 }
 
 func NewNodeCache(depth int, fdepth int, path string) *NodeCache {
-	offset := sizeByDepth(depth)
-	size := sizeByDepth(depth + fdepth)
-	var f *os.File
-	if fdepth > 0 {
-		var err error
-		if f, err = openfile(path); err != nil {
-			log.Infof("NodeCache fdepth:%d will be ignored, err:%+v", fdepth, err)
-			size = offset
-		} else {
-			abs, _ := filepath.Abs(f.Name())
-			log.Debugf("NodeCache using fdepth:%d path:%s", fdepth, abs)
-		}
-	}
+	bc := NewBranchCache(depth, fdepth, path)
 	return &NodeCache{
-		nodes:  make([][2][]byte, offset),
-		offset: offset,
-		depth:  depth+fdepth,
-		size:   size,
-		f:      f,
+		impl: bc,
 	}
 }
