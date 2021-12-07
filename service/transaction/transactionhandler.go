ee724ea6dfdf38a4139ae81878bcbc81cb7d9709
Jaechang Namgoong <jnamgoong@iconloop.com>
2021-12-07 13:53:50 +0900

    Fix the incorrect arguments position

--- service/transaction/transactionhandler.go
+++ service/transaction/transactionhandler.go
@@ -173,7 +173,7 @@ func (th *transactionHandler) Execute(ctx contract.Context, estimate bool) (txre
 	fid := cc.FrameID()
 	th.cc = cc
 	logger := trace.LoggerOf(cc.Logger())
-	logger.TSystemf("FRAME[%d] TRANSACTION start from=%s to=%s id=%#x", fid, th.to, th.from, th.cc.TransactionID())
+	logger.TSystemf("FRAME[%d] TRANSACTION start from=%s to=%s id=%#x", fid, th.from, th.to, th.cc.TransactionID())
 
 	status, addr, err := th.DoExecute(cc, estimate, isPatch)
 	if err != nil {
