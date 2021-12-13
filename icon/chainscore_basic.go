47f5643408a1ccc9e1368749e02bcdd80aef2682
Eunsoo Park <eunsoo.park@iconloop.com>
2021-12-13 12:26:40 +0900

    Set default slash ratio of Non Vote Penalty at Revision 16

--- icon/chainscore_basic.go
+++ icon/chainscore_basic.go
@@ -299,6 +299,10 @@ func (s *chainScore) handleRevisionChange(as state.AccountState, r1, r2 int) err
 			if err := scoredb.NewVarDB(as, state.VarEnabledEETypes).Set(EETypesJavaAndPython); err != nil {
 				return err
 			}
+		}
+
+		// Set slash ratio of Non Vote Penalty
+		if r1 < icmodule.RevisionICON2R3 && r2 >= icmodule.RevisionICON2R3 {
 			iconConfig := s.loadIconConfig()
 			if err := es.State.SetNonvotedPenaltySlashRatio(int(iconConfig.NonVotePenaltySlashRatio.Int64())); err != nil {
 				return err
