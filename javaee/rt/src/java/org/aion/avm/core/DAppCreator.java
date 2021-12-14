b4b54d111ab143cfdefbd986f7fa98a76263c315
Hyunil Jung <hijung@iconloop.com>
2021-12-14 17:37:05 +0900

    Log one stack trace in one log entry

--- javaee/rt/src/java/org/aion/avm/core/DAppCreator.java
+++ javaee/rt/src/java/org/aion/avm/core/DAppCreator.java
@@ -6,7 +6,6 @@ import foundation.icon.ee.types.Result;
 import foundation.icon.ee.types.Status;
 import foundation.icon.ee.types.Transaction;
 import foundation.icon.ee.util.LogMarker;
-import foundation.icon.ee.util.StringConsumerOutputStream;
 import i.AvmError;
 import i.AvmException;
 import i.AvmThrowable;
@@ -22,6 +21,7 @@ import org.aion.parallel.TransactionTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 public class DAppCreator {
@@ -84,10 +84,9 @@ public class DAppCreator {
             if (conf.testMode) {
                 e.printStackTrace();
             }
-            var os = new StringConsumerOutputStream(s -> {
-                logger.trace(LogMarker.Trace, s);
-            });
-            e.printStackTrace(new PrintStream(os));
+            var bos = new ByteArrayOutputStream();
+            e.printStackTrace(new PrintStream(bos));
+            logger.trace(LogMarker.Trace, bos.toString());
             long stepUsed = (runtimeSetup != null) ?
                     (tx.getLimit() - IInstrumentation.getEnergyLeft()) : 0;
             result = new Result(e.getCode(), stepUsed, e.getResultMessage());
