f30bb4fdf7d8ded1e7bc57b11df5d770c3b1209f
Hyunil Jung <hijung@iconloop.com>
2022-07-18 17:12:15 +0900

    Fix wrong icon address conversion from pub key

--- btp/ntm/iconmodule_test.go
+++ btp/ntm/iconmodule_test.go
@@ -27,7 +27,7 @@ func TestIconModule_newAddressFromPubKey(t *testing.T) {
 	assert := assert.New(t)
 	pk, err := hex.DecodeString("04f309c682bf0d5cc2099e80ad71ed872731138fc9c4df2b38997a9fd4811ed23b7e64777ba926cd51e8ee621ce8f7940c2a091d3480fd7207308c840148e93556")
 	assert.NoError(err)
-	expAddr, err := hex.DecodeString("005e1e719a335af4f31e6f3f7bd29b6fda1db56a4d")
+	expAddr, err := hex.DecodeString("5e1e719a335af4f31e6f3f7bd29b6fda1db56a4d")
 	assert.NoError(err)
 	addr, err := newIconAddressFromPubKey(pk)
 	assert.NoError(err)
