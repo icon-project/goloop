05a40f3c5ebf5ef937885b48340e8ce02d890b05
Hyunil Jung <hijung@iconloop.com>
2022-10-19 15:57:08 +0900

    Revise bitArray test

--- consensus/bitarray_test.go
+++ consensus/bitarray_test.go
@@ -2,9 +2,12 @@ package consensus
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestBitArrayPickRandom(t *testing.T) {
+func TestBitArray_PickRandom(t *testing.T) {
+	assert := assert.New(t)
 	testCases := []struct {
 		len    int
 		ones   []int
@@ -24,46 +27,71 @@ func TestBitArrayPickRandom(t *testing.T) {
 		ba := newBitArray(c.len)
 		for i := 0; i < len(c.ones); i++ {
 			ba.Set(c.ones[i])
-			if !ba.Get(c.ones[i]) {
-				t.Errorf("not set\n")
-			}
+			assert.True(ba.Get(c.ones[i]))
 		}
 		for i := 0; i < c.repeat; i++ {
 			v := ba.PickRandom()
-			if v != c.expect {
-				t.Errorf("bad random value ones:%v expected:%v actual:%v\n", c.ones, c.expect, v)
-			}
+			assert.Equal(c.expect, v)
 		}
 	}
 }
 
-func TestBitArrayPickRandom2(t *testing.T) {
+func TestBitArray_PickRandom2(t *testing.T) {
+	assert := assert.New(t)
 	testCases := []struct {
-		len    int
-		ones   []int
-		repeat int
+		len  int
+		ones []int
 	}{
-		{100, []int{1, 2, 3, 4, 5}, 1000},
-		{100, []int{1, 2, 3, 4, 5, 63, 64, 77}, 1000},
-		{500, []int{64, 77, 399}, 1000},
+		{100, []int{}},
+		{100, []int{1}},
+		{100, []int{31}},
+		{100, []int{63}},
+		{100, []int{1, 2, 3, 4, 5}},
+		{100, []int{1, 2, 3, 4, 5, 63, 64, 77}},
+		{500, []int{64, 77, 399}},
 	}
 	for _, c := range testCases {
 		ba := newBitArray(c.len)
 		for i := 0; i < len(c.ones); i++ {
 			ba.Set(c.ones[i])
+			assert.True(ba.Get(c.ones[i]))
 		}
+		baCopy := ba.Copy()
 		ba2 := newBitArray(c.len)
-		for i := 0; i < c.repeat; i++ {
+		for i := 0; i < len(c.ones); i++ {
 			v := ba.PickRandom()
-			if !ba.Get(v) {
-				t.Errorf("bad random value ones:%v random:%v\n", c.ones, v)
-			}
+			assert.True(ba.Get(v))
+			assert.False(ba2.Get(v))
+			ba.Unset(v)
 			ba2.Set(v)
 		}
-		for i := 0; i < len(ba.Words); i++ {
-		}
-		if !ba.Equal(ba2) {
-			t.Errorf("not random enough, expected:%v random:%v\n", ba, ba2)
-		}
+		assert.Equal(-1, ba.PickRandom())
+		assert.True(baCopy.Equal(ba2))
 	}
+}
+
+func TestBitArray_Basics(t *testing.T) {
+	assert := assert.New(t)
+	const bits = 10
+	ba := newBitArray(bits)
+
+	assert.EqualValues(bits, ba.Len())
+	assert.EqualValues(false, ba.Get(2))
+	ba.Put(2, true)
+	assert.EqualValues(true, ba.Get(2))
+	ba.Put(2, false)
+	assert.EqualValues(false, ba.Get(2))
+	assert.EqualValues(false, ba.Get(11))
+
+	ba2 := newBitArray(bits + 1)
+	ba2.AssignAnd(ba)
+	assert.EqualValues(bits, ba2.Len())
+
+	ba2 = newBitArray(bits + 1)
+	assert.False(ba.Equal(ba2))
+
+	ba2 = ba.Copy()
+	assert.True(ba.Equal(ba2))
+	ba2.Set(3)
+	assert.False(ba.Equal(ba2))
 }
