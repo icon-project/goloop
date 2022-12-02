43c8ab927a31c5f54435b06fbc8b33eae2d2c612
Hyunil Jung <hijung@iconloop.com>
2022-12-02 10:28:50 +0900

    Refactor ictest: move test.WAL to consensus.testWAL

--- icon/ictest/fixtureoption.go
+++ icon/ictest/fixtureoption.go
@@ -143,7 +143,7 @@ func UseCSForBlockV1(cf *test.FixtureConfig) *test.FixtureConfig {
 			header, lastVotes, err := iplt.GetBlockV1Proof()
 			assert.NoError(t, err)
 			wal := path.Join(ctx.Base, "wal")
-			wm := test.NewWAL()
+			wm := consensus.NewTestWAL()
 			cs, err := icconsensus.New(
 				ctx.C,
 				wal,
