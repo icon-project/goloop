719ab40fa7b0c7a81ea18dc4129ca6ef18420a16
Chiwon Cho <superdoli@gmail.com>
2023-06-19 16:22:46 +0900

    Fix bugs in feepayerinfo.go
    * Fix a problem in addBigInt()
    * Use addBigInt() in FeePayerInfo.GetLogs()

--- service/contract/feepayerinfo.go
+++ service/contract/feepayerinfo.go
@@ -180,7 +180,7 @@ func addBigInt(v1, v2 *big.Int) *big.Int {
 	if v1 == nil {
 		return v2
 	} else if v2 == nil {
-		return nil
+		return v1
 	} else {
 		return new(big.Int).Add(v1, v2)
 	}
@@ -196,7 +196,7 @@ func (p *FeePayerInfo) GetLogs(r txresult.Receipt) bool {
 		if p2, ok := m[key]; ok {
 			m[key] = &FeePayer{
 				payer:     p1.payer,
-				paidSteps: new(big.Int).Add(p1.paidSteps, p2.paidSteps),
+				paidSteps: addBigInt(p1.paidSteps, p2.paidSteps),
 				feeSteps:  addBigInt(p1.feeSteps, p2.feeSteps),
 			}
 		} else {
