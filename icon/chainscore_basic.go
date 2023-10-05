ce1d35323a6aa86660b21531369d9fd587a2b393
Eunsoo Park <eunsoo.park@parametacorp.com>
2023-10-05 13:42:08 +0900

    Fix to drop TX of blocked address in Revision13

--- icon/chainscore_basic.go
+++ icon/chainscore_basic.go
@@ -111,29 +111,7 @@ func (s *chainScore) handleRevisionChange(r1, r2 int) error {
 }
 
 func (s *chainScore) blockAccounts() {
-	targets := []string{
-		"hx76dcc464a27d74ca7798dd789d2e1da8193219b4",
-		"hxac5c6e6f7a6e8ae1baba5f0cb512f7596b95f1fe",
-		"hx966f5f9e2ab5b80a0f2125378e85d17a661352f4",
-		"hxad2bc6446ee3ae23228889d21f1871ed182ca2ca",
-		"hxc39a4c8438abbcb6b49de4691f07ee9b24968a1b",
-		"hx96505aac67c4f9033e4bac47397d760f121bcc44",
-		"hxf5bbebeb7a7d37d2aee5d93a8459e182cbeb725d",
-		"hx4602589eb91cf99b27296e5bd712387a23dd8ce5",
-		"hxa67e30ec59e73b9e15c7f2c4ddc42a13b44b2097",
-		"hx52c32d0b82f46596f697d8ba2afb39105f3a6360",
-		"hx985cf67b563fb908543385da806f297482f517b4",
-		"hxc0567bbcba511b84012103a2360825fddcd058ab",
-		"hx20be21b8afbbc0ba46f0671508cfe797c7bb91be",
-		"hx19e551eae80f9b9dcfed1554192c91c96a9c71d1",
-		"hx0607341382dee5e039a87562dcb966e71881f336",
-		"hxdea6fe8d6811ec28db095b97762fdd78b48c291f",
-		"hxaf3a561e3888a2b497941e464f82fd4456db3ebf",
-		"hx061b01c59bd9fc1282e7494ff03d75d0e7187f47",
-		"hx10d12d5726f50e4cf92c5fad090637b403516a41",
-		"hx10e8a7289c3989eac07828a840905344d8ed559b",
-	}
-	for _, target := range targets {
+	for target, _ := range icmodule.BlockedAccount {
 		addr := common.MustNewAddressFromString(target)
 		as := s.cc.GetAccountState(addr.ID())
 		as.SetBlock(true)
--- icon/icmodule/constant.go
+++ icon/icmodule/constant.go
@@ -97,3 +97,26 @@ var (
 
 	DefaultMinBond = new(big.Int).Mul(big.NewInt(10_000), BigIntICX)
 )
+
+var BlockedAccount = map[string]bool{
+	"hx76dcc464a27d74ca7798dd789d2e1da8193219b4": true,
+	"hxac5c6e6f7a6e8ae1baba5f0cb512f7596b95f1fe": true,
+	"hx966f5f9e2ab5b80a0f2125378e85d17a661352f4": true,
+	"hxad2bc6446ee3ae23228889d21f1871ed182ca2ca": true,
+	"hxc39a4c8438abbcb6b49de4691f07ee9b24968a1b": true,
+	"hx96505aac67c4f9033e4bac47397d760f121bcc44": true,
+	"hxf5bbebeb7a7d37d2aee5d93a8459e182cbeb725d": true,
+	"hx4602589eb91cf99b27296e5bd712387a23dd8ce5": true,
+	"hxa67e30ec59e73b9e15c7f2c4ddc42a13b44b2097": true,
+	"hx52c32d0b82f46596f697d8ba2afb39105f3a6360": true,
+	"hx985cf67b563fb908543385da806f297482f517b4": true,
+	"hxc0567bbcba511b84012103a2360825fddcd058ab": true,
+	"hx20be21b8afbbc0ba46f0671508cfe797c7bb91be": true,
+	"hx19e551eae80f9b9dcfed1554192c91c96a9c71d1": true,
+	"hx0607341382dee5e039a87562dcb966e71881f336": true,
+	"hxdea6fe8d6811ec28db095b97762fdd78b48c291f": true,
+	"hxaf3a561e3888a2b497941e464f82fd4456db3ebf": true,
+	"hx061b01c59bd9fc1282e7494ff03d75d0e7187f47": true,
+	"hx10d12d5726f50e4cf92c5fad090637b403516a41": true,
+	"hx10e8a7289c3989eac07828a840905344d8ed559b": true,
+}
