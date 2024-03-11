ba1f4a7e6c7b9e1828410d317299f71b5a790432
Eunsoo Park <eunsoo.park@parametacorp.com>
2024-03-11 15:38:24 +0900

    Add `count` to calculator.Stats

--- icon/iiss/calculator/statistics.go
+++ icon/iiss/calculator/statistics.go
@@ -24,6 +24,7 @@ import (
 
 type Stats struct {
 	value map[RewardType]*big.Int
+	count map[RewardType]int64
 }
 
 func (s *Stats) GetValue(t RewardType) *big.Int {
@@ -34,11 +35,21 @@ func (s *Stats) GetValue(t RewardType) *big.Int {
 	}
 }
 
+func (s *Stats) GetCount(t RewardType) int64 {
+	if v, ok := s.count[t]; ok {
+		return v
+	} else {
+		return 0
+	}
+}
+
 func (s *Stats) IncreaseReward(t RewardType, amount *big.Int) {
 	if v, ok := s.value[t]; ok {
 		s.value[t] = new(big.Int).Add(v, amount)
+		s.count[t]++
 	} else {
 		s.value[t] = amount
+		s.count[t] = 1
 	}
 }
 
@@ -54,11 +65,14 @@ func (s *Stats) String() string {
 	var sb strings.Builder
 	fmt.Fprintf(&sb, "Total=%d", s.Total())
 	for k, v := range s.value {
-		fmt.Fprintf(&sb, " %s=%d", k, v)
+		fmt.Fprintf(&sb, " %s: (count=%d, value=%s)", k, s.count[k], v)
 	}
 	return sb.String()
 }
 
 func NewStats() *Stats {
-	return &Stats{value: make(map[RewardType]*big.Int)}
+	return &Stats{
+		value: make(map[RewardType]*big.Int),
+		count: make(map[RewardType]int64),
+	}
 }
