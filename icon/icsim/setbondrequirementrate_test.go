2f4649a34c874e31ba1f812c0f16e03c6fdf02a4
Chiwon Cho <superdoli@gmail.com>
2024-05-27 17:18:54 +0900

    Add a testcase for setBondRequirementRate() API to icsim.Simulator

---
+++ icon/icsim/setbondrequirementrate_test.go
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2024 ICON Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package icsim
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/icon-project/goloop/icon/icmodule"
+	"github.com/icon-project/goloop/module"
+)
+
+func assertBondRequirement(t *testing.T, sim Simulator, br, nextBr icmodule.Rate) {
+	jso, err := sim.GetBondRequirementRate()
+	assert.NoError(t, err)
+	assert.Equal(t, map[string]interface{}{
+		"blockHeight": sim.BlockHeight(), "current": br, "next": nextBr,
+	}, jso)
+
+	term := sim.GetPRepTermInJSON()
+	assert.Equal(t, br.Percent(), term["bondRequirement"])
+
+	networkInfo := sim.GetNetworkInfoInJSON()
+	assert.Equal(t, br.Percent(), networkInfo["bondRequirement"])
+}
+
+func TestSimulatorImpl_SetBondRequirementRate(t *testing.T) {
+	const (
+		termPeriod = int64(10)
+	)
+	var err error
+	var csi module.ConsensusInfo
+	var receipts []Receipt
+	var nextBr icmodule.Rate
+	var term map[string]interface{}
+	var networkInfo map[string]interface{}
+	br := icmodule.ToRate(5)
+	rev := icmodule.ValueToRevision(icmodule.RevisionSetBondRequirementRate - 1)
+
+	cfg := NewSimConfigWithParams(map[SimConfigOption]interface{}{
+		SCOTermPeriod:      termPeriod,
+		SCOBondRequirement: br,
+	})
+	env, err := NewEnv(cfg, rev)
+	sim := env.Simulator()
+	assert.NoError(t, err)
+	assert.NotNil(t, sim)
+	assert.Equal(t, sim.Revision().Value(), rev.Value())
+
+	// T(0)
+	jso := sim.GetNetworkInfoInJSON()
+	assert.Equal(t, br.Percent(), jso["bondRequirement"])
+
+	term = sim.GetPRepTermInJSON()
+	assert.Equal(t, br.Percent(), term["bondRequirement"])
+
+	// GetBondRequirementRate is not allowed before icmodule.RevisionSetBondRequirementRate
+	jso, err = sim.GetBondRequirementRate()
+	assert.Error(t, err)
+	assert.Nil(t, jso)
+
+	assert.NoError(t, sim.GoToTermEnd(csi))
+
+	// T(1)
+	// SetBondRequirementRate is forbidden before icmodule.RevisionSetBondRequirementRate
+	receipts, err = sim.GoBySetBondRequirementRate(csi, env.Governance(), icmodule.ToRate(3))
+	assert.NoError(t, err)
+	assert.Equal(t, Failure, receipts[1].Status())
+
+	// GetBondRequirementRate is forbidden before icmodule.RevisionSetBondRequirementRate
+	jso, err = sim.GetBondRequirementRate()
+	assert.Error(t, err)
+	assert.Nil(t, jso)
+
+	term = sim.GetPRepTermInJSON()
+	assert.Equal(t, br.Percent(), term["bondRequirement"])
+
+	networkInfo = sim.GetNetworkInfoInJSON()
+	assert.Equal(t, br.Percent(), networkInfo["bondRequirement"])
+
+	// Revision update to RevisionSetBondRequirementRate
+	rev = icmodule.ValueToRevision(icmodule.RevisionSetBondRequirementRate)
+	receipts, err = sim.GoBySetRevision(csi, env.Governance(), rev)
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(receipts))
+	assert.Equal(t, Success, receipts[1].Status())
+	assert.Equal(t, sim.Revision().Value(), rev.Value())
+
+	// GetBondRequirementRate() works after RevisionSetBondRequirementRate
+	jso, err = sim.GetBondRequirementRate()
+	assert.NoError(t, err)
+	assert.Equal(t, map[string]interface{}{
+		"blockHeight": sim.BlockHeight(), "current": br, "next": br,
+	}, jso)
+
+	// Ensure that calling setBondRequirementRate() more than once during the same term works well
+	for i := 0; i < 2; i++ {
+		nextBr = icmodule.ToRate(int64(i))
+		receipts, err = sim.GoBySetBondRequirementRate(csi, env.Governance(), nextBr)
+		assert.NoError(t, err)
+		assert.Equal(t, 2, len(receipts))
+		assert.Equal(t, Success, receipts[1].Status())
+
+		assertBondRequirement(t, sim, br, nextBr)
+	}
+
+	assert.NoError(t, sim.GoToTermEnd(csi))
+
+	// T(2)
+	jso, err = sim.GetBondRequirementRate()
+	assert.NoError(t, err)
+	assert.Equal(t, map[string]interface{}{
+		"blockHeight": sim.BlockHeight(), "current": nextBr, "next": nextBr,
+	}, jso)
+
+	assert.NoError(t, sim.Go(csi, 1))
+	assertBondRequirement(t, sim, nextBr, nextBr)
+
+	assert.NoError(t, sim.GoToTermEnd(csi))
+
+	// T(3)
+	assertBondRequirement(t, sim, nextBr, nextBr)
+
+	// Ensure that only valid BondRequirementRates are allowed
+	for _, rate := range []icmodule.Rate{-1, icmodule.ToRate(101)} {
+		receipts, err = sim.GoBySetBondRequirementRate(csi, env.Governance(), rate)
+		assert.NoError(t, err)
+		assert.Equal(t, 2, len(receipts))
+		assert.Equal(t, Failure, receipts[1].Status())
+
+		assertBondRequirement(t, sim, nextBr, nextBr)
+	}
+}
